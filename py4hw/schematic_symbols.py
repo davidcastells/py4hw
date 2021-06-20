9a8333ef1b298f6866eff5afee6fa882bfaa8bc7
U-TPY14\dcr <david.castells@uab.cat>
2021-06-20 23:29:30 +0200

    single class for add and sub

--- py4hw/schematic_symbols.py
+++ py4hw/schematic_symbols.py
@@ -69,7 +69,7 @@ class LogicSymbol:
     def getOccupancy(self):
         return {'x':self.x, 'y':self.y, 'w':self.getWidth(), 'h':self.getHeight()}
 
-class AddSymbol(LogicSymbol):
+class BinaryOperatorSymbol(LogicSymbol):
     def __init__(self, obj, x, y):
         super().__init__(obj, x, y)
         
@@ -80,7 +80,7 @@ class AddSymbol(LogicSymbol):
         canvas.drawText(x, y, text=self.obj.name, anchor='w')
         y = y + namemargin
 
-        canvas.drawText(x+25, y+25+5, '+', anchor='c')
+        canvas.drawText(x+25, y+25+5, self.operator, anchor='c')
         canvas.drawEllipse(x, y, x+50, y+50)
         #canvas.drawLine(x, y, x + 40, y)
         #canvas.drawLine(x, y + 40, x + 40, y + 40)
@@ -113,31 +113,59 @@ class AddSymbol(LogicSymbol):
 
         return (25-x, namemargin + 25 + y)
 
+
+class AddSymbol(BinaryOperatorSymbol):
+    def __init__(self, obj, x, y):
+        super().__init__(obj, x, y)
+        self.operator = '+'
+
+class SubSymbol(BinaryOperatorSymbol):
+    def __init__(self, obj, x, y):
+        super().__init__(obj, x, y)
+        self.operator = '-'
     
 class AndSymbol(LogicSymbol):
     def __init__(self, obj, x, y):
         super().__init__(obj, x, y)
+        self.h = 40
 
     def draw(self, canvas):
         x = self.x
         y = self.y
 
         canvas.drawText(x, y, text=self.obj.name, anchor='w')
-        y = y + 8
+        y = y + namemargin
 
-        y = y + 10
-
-        canvas.drawLine(x, y, x + 40, y)
-        canvas.drawLine(x, y + 40, x + 40, y + 40)
-        canvas.drawLine(x, y, x, y + 40)
-        canvas.drawArc(x + 20, y, x + 50, y + 40, start=-90, extent=180) #, style=tkinter.ARC, outline='black', fill='white')
+        # the and box would be x[0:50] y[0:30]
+        canvas.drawLine(x, y, x + 25, y)
+        canvas.drawLine(x, y + self.h, x + 25, y + self.h)
+        canvas.drawLine(x, y, x, y + self.h)
+        canvas.drawArc(x , y, x + 50, y + self.h, start=-90, extent=90) #, style=tkinter.ARC, outline='black', fill='white')
 
     def getHeight(self):
-        return 30
+        return namemargin + self.h
 
     def getWidth(self):
         return 50
     
+    def getWireSourcePos(self, wire:Wire):
+        return (self.getWidth(), namemargin + self.h//2)
+    
+    def getWireSinkPos(self, wire:Wire):
+        selidx = -1
+        for idx, port in enumerate(self.obj.inPorts):
+            if (port.wire == wire):
+                selidx = idx
+                
+        if (selidx == -1):
+            raise Exception('in port not found in {}'.format(self.obj.getFullPath()) )
+
+        if (selidx == 0):
+            y = self.h//2 - 10
+        else:
+            y = self.h//2 + 10 
+
+        return (0, namemargin + y)
 
 class NotSymbol(LogicSymbol):
     def __init__(self, obj, x, y):
@@ -166,26 +194,48 @@ class NotSymbol(LogicSymbol):
 class OrSymbol(LogicSymbol):
     def __init__(self, obj, x, y):
         super().__init__(obj, x, y)
+        self.h = 40
 
     def draw(self, canvas):
         x = self.x
         y = self.y
 
-        canvas.create_text(x, y, text=self.obj.name, anchor='w')
-        y = y + namemargin 
+        canvas.drawText(x, y, text=self.obj.name, anchor='w')
+        y = y + namemargin
 
-        y = y + 10
+        # the and box would be x[0:50] y[0:30]
+        canvas.drawLine(x, y, x + 20, y)
+        canvas.drawLine(x, y + self.h, x + 20, y + self.h)
+        canvas.drawArc(x-10 , y, x + 10, y + self.h, start=-90, extent=90) #, style=tkinter.ARC, outline='black', fill='white')
 
-        canvas.drawLine(x, y, x + 40, y)
-        canvas.drawLine(x, y + 40, x + 40, y + 40)
-        canvas.drawLine(x, y, x, y + 40)
-        #canvas.drawArc(x + 20, y, x + 50, y + 40, start=-90, extent=180, style=tkinter.ARC, outline='black', fill='white')
+        #canvas.drawLine(x, y, x, y + self.h)
+        canvas.drawArc(x-30 , y, x + 50+20, y + self.h*4, start=-90, extent=-60) #, style=tkinter.ARC, outline='black', fill='white')
+        canvas.drawArc(x-30 , y-self.h*3, x + 50+20, y + self.h, start=60, extent=90) #, style=tkinter.ARC, outline='black', fill='white')
 
     def getHeight(self):
-        return 30
+        return namemargin + self.h
 
     def getWidth(self):
         return 50
+    
+    def getWireSourcePos(self, wire:Wire):
+        return (self.getWidth(), namemargin + self.h//2)
+    
+    def getWireSinkPos(self, wire:Wire):
+        selidx = -1
+        for idx, port in enumerate(self.obj.inPorts):
+            if (port.wire == wire):
+                selidx = idx
+                
+        if (selidx == -1):
+            raise Exception('in port not found in {}'.format(self.obj.getFullPath()) )
+
+        if (selidx == 0):
+            y = self.h//2 - 10
+        else:
+            y = self.h//2 + 10 
+
+        return (5, namemargin + y)
     
 class InPortSymbol(LogicSymbol):
     def __init__(self, obj, x, y):
