e8c045c44a7de6cc0d1bb4a1e258e04deed96861
David Castells-Rufas <david.castells@uab.cat>
2022-01-18 17:54:03 +0100

    unit testing

--- tests/tb_Add.py
+++ tests/tb_Add.py
@@ -2,30 +2,48 @@
 from py4hw.base import *
 from py4hw.logic import *
 import py4hw.debug
+import unittest
 
-sys = HWSystem()
+class tb_Add(unittest.TestCase):
+    
+    def test1(self):
+        sys = HWSystem()
+        a = sys.wire("a", 32)
+        b = sys.wire("b", 32)
+        r1 = sys.wire("r", 32)
+        Constant(sys, "a", 10, a)
+        Constant(sys, "b", 20, b)
+        Add(sys, "add1", a,b, r1)
+        sys.getSimulator().clk(1);
+        self.assertEqual(r1.get(), 30)
+        
+    def test_Integrity(self):
+        sys = HWSystem()
+        
+        a = sys.wire("a", 32)
+        b = sys.wire("b", 32)
+        c = sys.wire("c", 32)
+        r1 = sys.wire("r", 32)
+        r2 = sys.wire("r2", 32)
+        
+        Add(sys, "add1", a,b, r1)
+        Add(sys, "add2", r1, c, r2)
+        
+        Constant(sys, "a", 10, a)
+        Constant(sys, "b", 20, b)
+        Constant(sys, "c", 5, c)
+        Scope(sys, "r2", r2)
+        
+        py4hw.debug.checkIntegrity(sys)
+        # py4hw.debug.printHierarchy(sys)
+        
+        # print('RESET')
+        # sim = sys.getSimulator()
+        
+        # print()
+        # print('CLK')
+        # sim.clk(1)
 
-a = sys.wire("a", 32)
-b = sys.wire("b", 32)
-c = sys.wire("c", 32)
-r1 = sys.wire("r", 32)
-r2 = sys.wire("r2", 32)
 
-Add(sys, "add1", a,b, r1)
-Add(sys, "add2", r1, c, r2)
-
-Constant(sys, "a", 10, a)
-Constant(sys, "b", 20, b)
-Constant(sys, "c", 5, c)
-Scope(sys, "r2", r2)
-
-py4hw.debug.printHierarchy(sys)
-
-print('RESET')
-sim = sys.getSimulator()
-
-print()
-print('CLK')
-sim.clk(1)
-
-
+if __name__ == '__main__':
+    unittest.main()
