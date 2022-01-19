71db32676c1daefeb1039223265996f2b0f4766f
David Castells-Rufas <david.castells@uab.cat>
2022-01-19 17:03:38 +0100

    repackaging

--- tests/Test_Minterm.py
+++ tests/Test_Minterm.py
@@ -11,9 +11,7 @@ Created on Sat Oct 31 11:19:05 2020
 
 @author: dcr
 """
-from py4hw.base import *
-from py4hw.logic import *
-from py4hw.simulation import Simulator
+import py4hw
 import py4hw.debug
 import pytest
 
@@ -21,28 +19,28 @@ import pytest
 class Test_Minterm:
 
     def test_integrity(self):
-        sys = HWSystem()
+        sys = py4hw.HWSystem()
         
         a = sys.wire("a", 2)
         
         bits = sys.wires('b', 2, 1)
         minterm = sys.wire('minterm',1)
         
-        Bits(sys, 'bits', a, bits)
-        Minterm(sys, 'minterm', bits, 0, minterm)
-        Constant(sys, "a", 0, a)
+        py4hw.Bits(sys, 'bits', a, bits)
+        py4hw.Minterm(sys, 'minterm', bits, 0, minterm)
+        py4hw.Constant(sys, "a", 0, a)
         
         for i in range(len(bits)):
-            Scope(sys, 'b{}'.format(i), bits[i])
+            py4hw.Scope(sys, 'b{}'.format(i), bits[i])
         
-        Scope(sys, 'minterm_5', minterm)
+        py4hw.Scope(sys, 'minterm_5', minterm)
         
         
         py4hw.debug.checkIntegrity(sys)
         
     def test_1(self):
         print('testing minterms equal to 5')
-        sys = HWSystem()
+        sys = py4hw.HWSystem()
         r = sys.wire("r", 1)
         a0 = sys.wire("a0", 1)
         a1 = sys.wire("a1", 1)
@@ -51,11 +49,11 @@ class Test_Minterm:
         exp_a0 = [0,1,0,1,0,1,0,1]
         exp_a1 = [0,0,1,1,0,0,1,1]
         exp_a2 = [0,0,0,0,1,1,1,1]
-        Sequence(sys, "a0", exp_a0, a0)
-        Sequence(sys, "a1", exp_a1, a1)
-        Sequence(sys, "a2", exp_a2, a2)
+        py4hw.Sequence(sys, "a0", exp_a0, a0)
+        py4hw.Sequence(sys, "a1", exp_a1, a1)
+        py4hw.Sequence(sys, "a2", exp_a2, a2)
 
-        Minterm(sys, 'minterm', [a0, a1, a2], 5, r)
+        py4hw.Minterm(sys, 'minterm', [a0, a1, a2], 5, r)
         
         for i in range(8):
             sys.getSimulator().clk(1)
