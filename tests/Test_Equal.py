25eaab5a7b271f6a45bb0f37ed728e201920f02d
David Castells-Rufas <david.castells@uab.cat>
2022-01-19 17:03:32 +0100

    repackaging

--- tests/Test_Equal.py
+++ tests/Test_Equal.py
@@ -1,6 +1,4 @@
-from py4hw.base import *
-from py4hw.logic import *
-from py4hw.simulation import Simulator
+import py4hw
 import py4hw.debug
 import pytest
 
@@ -8,27 +6,27 @@ class Test_Equal:
 
     def test_integrity(self):
 
-        sys = HWSystem()
+        sys = py4hw.HWSystem()
         
         a = sys.wire("a", 2)
         r = sys.wire("1", 1)
         
-        Constant(sys, "a", 0, a)
+        py4hw.Constant(sys, "a", 0, a)
         
-        Equal(sys, 'equal', a, 0, r)
+        py4hw.Equal(sys, 'equal', a, 0, r)
         
         py4hw.debug.checkIntegrity(sys)
 
     def test_1(self):
 
-        sys = HWSystem()
+        sys = py4hw.HWSystem()
         
         a = sys.wire("a", 2)
         r = sys.wire("1", 1)
         
-        Constant(sys, "a", 0, a)
+        py4hw.Constant(sys, "a", 0, a)
         
-        Equal(sys, 'equal', a, 0, r)
+        py4hw.Equal(sys, 'equal', a, 0, r)
         
         sys.getSimulator().clk(1)
         
@@ -36,14 +34,14 @@ class Test_Equal:
         
     def test_notEqual(self):
 
-        sys = HWSystem()
+        sys = py4hw.HWSystem()
         
         a = sys.wire("a", 2)
         r = sys.wire("1", 1)
         
-        Constant(sys, "a", 3, a)
+        py4hw.Constant(sys, "a", 3, a)
         
-        Equal(sys, 'equal', a, 0, r)
+        py4hw.Equal(sys, 'equal', a, 0, r)
         
         sys.getSimulator().clk(1)
         
@@ -52,14 +50,14 @@ class Test_Equal:
     def test_equal5(self):
 
         print('check values = 5')
-        sys = HWSystem()
+        sys = py4hw.HWSystem()
         
         a = sys.wire("a", 3)
         r = sys.wire("1", 1)
         
-        Sequence(sys, "a", [0,1,2,3,4,5,6,7], a)
+        py4hw.Sequence(sys, "a", [0,1,2,3,4,5,6,7], a)
         
-        Equal(sys, 'equal', a, 5, r)
+        py4hw.Equal(sys, 'equal', a, 5, r)
         
         for i in range(8):
             sys.getSimulator().clk(1)  
@@ -71,14 +69,14 @@ class Test_Equal:
             
     def test_oneBitWire(self):
         print('check values = 1')
-        sys = HWSystem()
+        sys = py4hw.HWSystem()
         
         a = sys.wire("a", 1)
         r = sys.wire("1", 1)
         
-        Sequence(sys, "a", [0,1], a)
+        py4hw.Sequence(sys, "a", [0,1], a)
         
-        Equal(sys, 'equal', a, 1, r)
+        py4hw.Equal(sys, 'equal', a, 1, r)
         
         for i in range(8):
             sys.getSimulator().clk(1)  
