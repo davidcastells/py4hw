d531669491082e23ac013e4d76afa1263e8b044b
David Castells-Rufas <david.castells@uab.cat>
2022-01-19 17:03:20 +0100

    repackaging

--- tests/Test_Add.py
+++ tests/Test_Add.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from py4hw.base import *
-from py4hw.logic import *
+import py4hw
 import py4hw.debug
 import pytest
 
@@ -8,18 +7,18 @@ class Test_Add:
     
     
     def test_1(self):
-        sys = HWSystem()
+        sys = py4hw.HWSystem()
         a = sys.wire("a", 32)
         b = sys.wire("b", 32)
         r1 = sys.wire("r", 32)
-        Constant(sys, "a", 10, a)
-        Constant(sys, "b", 20, b)
-        Add(sys, "add1", a,b, r1)
+        py4hw.Constant(sys, "a", 10, a)
+        py4hw.Constant(sys, "b", 20, b)
+        py4hw.Add(sys, "add1", a,b, r1)
         sys.getSimulator().clk(1);
         assert (r1.get() == 30)
         
     def test_Integrity(self):
-        sys = HWSystem()
+        sys = py4hw.HWSystem()
         
         a = sys.wire("a", 32)
         b = sys.wire("b", 32)
@@ -27,13 +26,13 @@ class Test_Add:
         r1 = sys.wire("r", 32)
         r2 = sys.wire("r2", 32)
         
-        Add(sys, "add1", a,b, r1)
-        Add(sys, "add2", r1, c, r2)
+        py4hw.Add(sys, "add1", a,b, r1)
+        py4hw.Add(sys, "add2", r1, c, r2)
         
-        Constant(sys, "a", 10, a)
-        Constant(sys, "b", 20, b)
-        Constant(sys, "c", 5, c)
-        Scope(sys, "r2", r2)
+        py4hw.Constant(sys, "a", 10, a)
+        py4hw.Constant(sys, "b", 20, b)
+        py4hw.Constant(sys, "c", 5, c)
+        py4hw.Scope(sys, "r2", r2)
         
         py4hw.debug.checkIntegrity(sys)
         # py4hw.debug.printHierarchy(sys)
