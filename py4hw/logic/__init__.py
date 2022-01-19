5614be591bb65936ede00a6e411e2d518032f494
David Castells-Rufas <david.castells@uab.cat>
2022-01-19 13:26:43 +0100

    new logic folder

---
+++ py4hw/logic/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 19 12:40:41 2022
+
+@author: dcr
+"""
+
+from .bitwise import *
+from .arithmetic import *
+from .relational import *
+from .simulation import *
+from .. import *
