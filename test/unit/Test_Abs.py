b463688734f8fda77eb565551308b682f52e8131
David Castells-Rufas <david.castells@uab.cat>
2022-02-03 07:32:31 +0100

    unit testing

---
+++ test/unit/Test_Abs.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  2 05:59:29 2022
+
+@author: dcr
+"""
+
+import py4hw
+import py4hw.debug
+import pytest
+
+
+class Test_Abs:
+    
+    def test_random(self):
+        sys = py4hw.HWSystem()
+        
+        a = sys.wire("a", 32)
+        r = sys.wire("r", 32)
+        
+        py4hw.Abs(sys, "abs", a, r)
+
+        import random
+        
+        for i in range(1000):
+            v = random.randint(-((1<<31)-1), (1<<31)-1)
+            a.put(v)
+        
+            sys.getSimulator().clk(1)
+        
+            assert(r.get() == abs(v))
+
+    def test_random_inv(self):
+        sys = py4hw.HWSystem()
+        
+        a = sys.wire("a", 32)
+        r = sys.wire("r", 32)
+        n = sys.wire("n")
+        
+        py4hw.Abs(sys, "abs", a, r, inverted=n)
+
+        import random
+        
+        for i in range(1000):
+            v = random.randint(-((1<<31)-1), (1<<31)-1)
+            a.put(v)
+        
+            sys.getSimulator().clk(1)
+        
+            assert(r.get() == abs(v))
+            assert(bool(n.get()) == (v<0))
+
+if __name__ == '__main__':
+    pytest.main(args=['-q', 'Test_Abs.py'])
