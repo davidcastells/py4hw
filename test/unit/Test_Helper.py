303b2a38ced50569f1bacb5590eaca7d2f4a4daa
David Castells-Rufas <david.castells@uab.cat>
2022-08-10 10:01:21 +0200

    test floating point helper

--- test/unit/Test_Helper.py
+++ test/unit/Test_Helper.py
@@ -28,5 +28,26 @@ class Test_Helper:
         for w in wl:
             print(w.getFullPath())
             
+    def test_floating_point(self):
+        import random
+        import math
+        
+        # special case for zero
+        sp = 0
+        sp = sp * math.pow(10, int(random.uniform(0, 37)))
+        ie = py4hw.helper.FloatingPointHelper.sp_to_ieee754(sp)
+        sp2 = py4hw.helper.FloatingPointHelper.ieee754_to_sp(ie)
+
+        assert(sp == sp2)
+        
+        for i in range(1000):
+            sp = random.uniform(-1, 1)
+            sp = sp * math.pow(10, int(random.uniform(0, 37)))
+            ie = py4hw.helper.FloatingPointHelper.sp_to_ieee754(sp)
+            sp2 = py4hw.helper.FloatingPointHelper.ieee754_to_sp(ie)
+            diff = abs(sp - sp2)/abs(sp)
+            #print('DIFF:', diff)
+            assert(diff < 1e-7)
+            
 if __name__ == '__main__':
     pytest.main(args=['-s', '-q', 'Test_Helper.py'])
