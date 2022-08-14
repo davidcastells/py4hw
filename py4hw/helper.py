f1f8c26aeebec137dd526cce1e3787c917aee96f
David Castells-Rufas <david.castells@uab.cat>
2022-08-14 11:20:39 +0200

    hw_equal_not_constant

--- py4hw/helper.py
+++ py4hw/helper.py
@@ -6,6 +6,7 @@ Created on Sat Jan 29 16:28:57 2022
 """
 
 from .base import Wire
+from .logic.arithmetic import *
 from .logic.bitwise import *
 from .logic.simulation import *
 
@@ -19,58 +20,18 @@ class LogicHelper:
         self.parent = parent
         self.inum = 0   # instance number
         
+    def hw_abs(self, a:Wire) -> Wire:
+        w = a.getWidth()
+        name = self._getNewName()
+        r = self.parent.wire(name, w)
+        Abs(self.parent, name, a, r)
+        return r
+
     def hw_and2(self, a:Wire, b:Wire) -> Wire:
         w = a.getWidth()
         name = self._getNewName()
         r = self.parent.wire(name, w)
         And2(self.parent, name, a, b, r)
-        return r
-
-    def hw_equal(self, a:Wire, b:Wire) -> Wire:
-        w = a.getWidth()
-        name = self._getNewName()
-        r = self.parent.wire(name)
-        Equal(self.parent, name, a, b, r)
-        return r
- 
-    def hw_equal_constant(self, a:Wire, b:int) -> Wire:
-        w = a.getWidth()
-        name = self._getNewName()
-        r = self.parent.wire(name)
-        EqualConstant(self.parent, name, a, b, r)
-        return r
-        
-    def _getNewName(self)->str:
-        str = 'i{}'.format(self.inum)
-        self.inum += 1
-        return str
-    
-    def hw_constant(self, width:int, v:int) -> Wire:
-        name = self._getNewName()
-        r = self.parent.wire(name, width)
-        Constant(self.parent, name, v, r)
-        return r
-    
-
-    def hw_or2(self, a:Wire, b:Wire) -> Wire:
-        w = a.getWidth()
-        name = self._getNewName()
-        r = self.parent.wire(name, w)
-        Or2(self.parent, name, a, b, r)
-        return r
-
-    def hw_or(self, a:list) -> Wire:
-        w = a[0].getWidth()
-        name = self._getNewName()
-        r = self.parent.wire(name, w)
-        Or(self.parent, name, a, r)
-        return r
-    
-    def hw_xor2(self, a:Wire, b:Wire) -> Wire:
-        w = a.getWidth()
-        name = self._getNewName()
-        r = self.parent.wire(name, w)
-        Xor2(self.parent, name, a, b, r)
         return r
 
     def hw_and3(self, a:Wire, b:Wire, c:Wire) -> Wire:
@@ -86,20 +47,114 @@ class LogicHelper:
         r = self.parent.wire(name, w)
         And(self.parent, name, [a, b, c, d], r)
         return r
-    
+
+    def hw_bit(self, a:Wire, bit:int) -> Wire:
+        name = self._getNewName()
+        r = self.parent.wire(name)
+        Bit(self.parent, name, a, bit, r)
+        return r
+
     def hw_buf(self, a:Wire) -> Wire:
         w = a.getWidth()
         name = self._getNewName()
         r = self.parent.wire(name, w)
         Buf(self.parent, name, a, r)
         return r
+
+    def hw_concatenate_msbf(self, ins:list) -> Wire:
+        name = self._getNewName()
+        w = 0
+        for inw in ins:
+            w += inw.getWidth()
+            
+        r = self.parent.wire(name, w)
+        ConcatenateMSBF(self.parent, name, ins, r)
+        return r
+
+    def hw_concatenate_lsbf(self, ins:list) -> Wire:
+        name = self._getNewName()
+        w = 0
+        for inw in ins:
+            w += inw.getWidth()
+            
+        r = self.parent.wire(name, w)
+        ConcatenateLSBF(self.parent, name, ins, r)
+        return r
     
-    def hw_not(self, a:Wire) -> Wire:
+    def hw_constant(self, width:int, v:int) -> Wire:
+        name = self._getNewName()
+        r = self.parent.wire(name, width)
+        Constant(self.parent, name, v, r)
+        return r
+
+    def hw_equal(self, a:Wire, b:Wire) -> Wire:
+        w = a.getWidth()
+        name = self._getNewName()
+        r = self.parent.wire(name)
+        Equal(self.parent, name, a, b, r)
+        return r
+ 
+    def hw_equal_constant(self, a:Wire, b:int) -> Wire:
+        w = a.getWidth()
+        name = self._getNewName()
+        r = self.parent.wire(name)
+        EqualConstant(self.parent, name, a, b, r)
+        return r
+            
+    def hw_not_equal_constant(self, a:Wire, b:int) -> Wire:
+        w = a.getWidth()
+        name = self._getNewName()
+        r = self.parent.wire(name)
+        NotEqualConstant(self.parent, name, a, b, r)
+        return r
+    
+    def hw_gt_constant(self, a:Wire, b:int) -> Wire:
+        w = a.getWidth()
+        gt = self.parent.wire(self._getNewName())
+        eq = self.parent.wire(self._getNewName())
+        lt = self.parent.wire(self._getNewName())
+        k = self.parent.wire(self._getNewName(), w)
+        Constant(self.parent, self._getNewName(), b, k)
+        Comparator(self.parent, self._getNewName(), a, k, gt, eq, lt)
+        return gt
+    
+    def _getNewName(self)->str:
+        while (True):
+            name = 'i{}'.format(self.inum)
+            self.inum += 1
+            if not(name in self.parent.children.keys()):
+                return name
+    
+    
+    def hw_or2(self, a:Wire, b:Wire) -> Wire:
         w = a.getWidth()
         name = self._getNewName()
         r = self.parent.wire(name, w)
-        Not(self.parent, name, a, r)
+        Or2(self.parent, name, a, b, r)
         return r
+
+    def hw_or(self, a:list) -> Wire:
+        w = a[0].getWidth()
+        name = self._getNewName()
+        r = self.parent.wire(name, w)
+        Or(self.parent, name, a, r)
+        return r
+
+    def hw_range(self, a:Wire, up:int, down:int) -> Wire:
+        w = up - down + 1
+        name = self._getNewName()
+        r = self.parent.wire(name, w)
+        Range(self.parent, name, a, up, down, r)
+        return r
+        
+    def hw_xor2(self, a:Wire, b:Wire) -> Wire:
+        w = a.getWidth()
+        name = self._getNewName()
+        r = self.parent.wire(name, w)
+        Xor2(self.parent, name, a, b, r)
+        return r
+
+    
         
     def hw_mux2(self, sel:Wire, s0:Wire, s1:Wire) -> Wire:
         w = s0.getWidth()
@@ -113,6 +168,34 @@ class LogicHelper:
         w = options[0].getWidth()
         r = self.parent.wire(name, w)
         Mux(self.parent, name, sel, options, r)
+        return r
+
+    def hw_neg(self, a:Wire) -> Wire:
+        w = a.getWidth()
+        name = self._getNewName()
+        r = self.parent.wire(name, w)
+        Neg(self.parent, name, a, r)
+        return r
+
+    def hw_not(self, a:Wire) -> Wire:
+        w = a.getWidth()
+        name = self._getNewName()
+        r = self.parent.wire(name, w)
+        Not(self.parent, name, a, r)
+        return r
+
+    def hw_sub(self, a:Wire, b:Wire) -> Wire:
+        w = a.getWidth()
+        name = self._getNewName()
+        r = self.parent.wire(name, w)
+        Sub(self.parent, name, a, b, r)
+        return r
+
+    def hw_sign(self, a:Wire) -> Wire:
+        w = 1
+        name = self._getNewName()
+        r = self.parent.wire(name, w)
+        Sign(self.parent, name, a, r)
         return r
     
     def sim_sequence(self, width:int, seq:list) -> Wire:
@@ -180,7 +263,7 @@ class FloatingPointHelper:
                 return 0.0
             
         ef = math.pow(2, e-127)
-        f = m / (1<<23)
+        f = ((1 << 23) | m) / (1<<23)
         
         if (s > 0):
             s = -1
@@ -199,7 +282,12 @@ class FloatingPointHelper:
         m = (v & ((1<<23)-1)) | (1<<23) 
         
         return FloatingPointHelper.ieee754_parts_to_sp(s, e, m)
-        
+
+    @staticmethod
+    def ieee754_stored_internally(v):
+        s,e,m = FloatingPointHelper.sp_to_ieee754_parts(v)
+        iv = FloatingPointHelper.ieee754_parts_to_sp(s, e, m)
+        return iv
 
     @staticmethod
     def zp_bin(v:int, vl:int) -> str:
