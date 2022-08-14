1feb53a72332550d1818d19ffcd5e09113d50749
David Castells-Rufas <david.castells@uab.cat>
2022-08-14 11:33:27 +0200

    FP to integer

--- py4hw/logic/arithmetic_fp.py
+++ py4hw/logic/arithmetic_fp.py
@@ -159,3 +159,133 @@ class FPAdder_SP(Logic):
         Range(self, 'mr3', mr2, 23, 1, mr3)
                 
         ConcatenateMSBF(self, 'final_r', [sr, er, mr3], r)
+        
+        
+        
+class FPtoInt_SP(Logic):
+    # This design is inspired in algorithm described in 
+    # section 9.2.1 of the book 
+    # Computer Principles and design in Verilog
+    def __init__(self, parent:Logic, name:str, a:Wire, r:Wire, p_lost:Wire, denorm:Wire, invalid:Wire):
+        super().__init__(parent, name)
+
+        self.addIn('a', a)
+        self.addOut('r', r)
+        self.addOut('p_lost', r)
+        self.addOut('denorm', denorm)
+        self.addOut('invalid', invalid)
+
+        from ..helper import LogicHelper
+        g = LogicHelper(self)
+        e = g.hw_range(a, 30, 23)
+        m = g.hw_range(a,22,0)
+        hidden_bit = g.hw_not_equal_constant(e, 0)        
+        frac_is_not_0 = g.hw_not_equal_constant(m, 0)
+
+        is_denorm = g.hw_and2(g.hw_not(hidden_bit), frac_is_not_0)
+        is_zero = g.hw_and2(g.hw_not(hidden_bit), g.hw_not(frac_is_not_0))
+        sign = g.hw_bit(a,31)
+        
+        zero_tail = g.hw_constant(23, 0)
+        real_m = g.hw_concatenate_msbf([hidden_bit, m]) 
+        #frac0 = g.hw_concatenate_msbf([hidden_bit, m, zero_tail]) # 56 bits
+        
+        # e2 = 32 + (e - 127)
+        real_e = g.hw_sub(e, g.hw_constant(8, 127))
+        
+        sign_real_e = g.hw_sign(real_e)
+        
+        k31 = g.hw_constant(real_e.getWidth(), 30)
+        #too_big = self.wire('too_big')
+        e_gt_31 = self.wire('e_gt_31')
+        e_eq_31 = self.wire('e_eq_31')
+        e_lt_31 = self.wire('e_lt_31')
+        Comparator(self, 'too_big', real_e, k31, e_gt_31, e_eq_31, e_lt_31)
+        too_big = g.hw_or2(g.hw_and2(e_gt_31, g.hw_not(sign_real_e)),
+                           g.hw_and2(sign, e_eq_31))
+        
+        # if sign is 1, this means that the number is < 0
+        
+        # 32 is the range of integers, 23 the range of the fractional part of m        
+        expanded_m = self.wire('expanded_m', 32+24) 
+        ShiftLeft(self, 'expanded_m', real_m, real_e, expanded_m)
+
+        integer_m = g.hw_range(expanded_m, 32+23-1, 23)
+        fractional_m = g.hw_range(expanded_m, 22, 0)
+        franctional_m_not_zero = g.hw_not_equal_constant(fractional_m, 0)
+        
+        positive_m = integer_m
+        negative_m = g.hw_neg(integer_m)
+        
+        final_m = g.hw_mux2(sign, positive_m, negative_m)
+        #wire [8:0] shift_right_bits = 9’d158 - {1’b0,a[30:23]}; // 127 + 31
+        
+        # for denorm values
+        select_denorm = is_denorm
+        p_lost_denorm = g.hw_constant(1,1)
+        invalid_denorm = g.hw_constant(1,0)
+        d_denorm = g.hw_constant(32, 0)
+        
+        # small values
+        select_small = sign_real_e
+        p_lost_small = frac_is_not_0 
+        invalid_small = g.hw_constant(1,0)
+        d_small = g.hw_constant(32, 0)
+        
+        # default
+        select_default = g.hw_or2(g.hw_not(is_denorm), g.hw_not(select_small))
+        p_lost_default = franctional_m_not_zero
+        invalid_default = too_big
+        d_default = final_m
+
+        Buf(self, 'denorm', is_denorm, denorm)
+        Select(self, 'select_p_lost', [select_denorm, select_small, select_default], 
+               [p_lost_denorm, p_lost_small, p_lost_default], p_lost)
+        Select(self, 'select_invalid', [select_denorm, select_small, select_default],
+               [invalid_denorm, invalid_small, invalid_default], invalid)
+        Select(self, 'select_d', [select_denorm, select_small, select_default], 
+               [d_denorm, d_small, d_default], r)
+
+
+class InttoFP_SP(Logic):
+    # This design is inspired in algorithm described in 
+    # section 9.2.2 of the book 
+    # Computer Principles and design in Verilog
+    def __init__(self, parent:Logic, name:str, a:Wire, r:Wire, p_lost:Wire):
+        super().__init__(parent, name)
+
+        assert(a.getWidth() == 32)        
+        assert(r.getWidth() == 32)        
+
+        self.addIn('a', a)
+        self.addOut('r', r)
+        self.addOut('p_lost', p_lost)
+
+        from ..helper import LogicHelper
+        g = LogicHelper(self)
+        
+        sign = self.wire('sign')
+        f5 = self.wire('f5', a.getWidth())
+        
+        Abs(self, 'f5', a, f5, inverted=sign)
+
+        clz = self.wire('clz', 5)
+        is_zero = self.wire('is_zero')
+        
+        CountLeadingZeros(self, 'clz', f5, clz, is_zero)
+
+        shifted = self.wire('shited', 32)
+        ShiftLeft(self, 'shift', f5, clz, shifted)
+        
+        # we skip the first 1, as it is implicit
+        fraction = g.hw_range(shifted, 30, 30-23+1)
+
+        pre_p_lost = g.hw_not_equal_constant(g.hw_range(shifted, 30-23, 0), 0)
+        Buf(self, 'p_lost', pre_p_lost, p_lost)
+
+        exponent = g.hw_sub(g.hw_constant(8, 127+31), clz)
+
+        #is_zero = g.hw_equal_constant(a, 0)
+        pre_r = self.wire('pre_r', 32)
+        ConcatenateMSBF(self, 'pre_r', [sign, exponent, fraction], pre_r)
+        Mux2(self, 'r', is_zero, pre_r, g.hw_constant(32, 0), r)
