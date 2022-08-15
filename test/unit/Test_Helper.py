54ed0056ec146418cfa23d65bbb37423756f3de0
David Castells-Rufas <david.castells@uab.cat>
2022-08-15 11:59:23 +0200

    parts module introduced

--- test/unit/Test_Helper.py
+++ test/unit/Test_Helper.py
@@ -11,7 +11,7 @@ import pytest
 class Test_Helper:
 
     def test_integrity(self):
-
+        print('integrity')
         sys = py4hw.HWSystem()
         
         a = sys.wire("a", 2)
@@ -31,10 +31,14 @@ class Test_Helper:
     def test_floating_point(self):
         import random
         import math
+
+        print('big range')
         
         # special case for zero
         sp = 0
         sp = sp * math.pow(10, int(random.uniform(0, 37)))
+        
+        print('sp=', sp)
         ie = py4hw.helper.FloatingPointHelper.sp_to_ieee754(sp)
         sp2 = py4hw.helper.FloatingPointHelper.ieee754_to_sp(ie)
 
@@ -43,10 +47,12 @@ class Test_Helper:
         for i in range(1000):
             sp = random.uniform(-1, 1)
             sp = sp * math.pow(10, int(random.uniform(0, 37)))
+            print('sp=', sp)
+
             ie = py4hw.helper.FloatingPointHelper.sp_to_ieee754(sp)
             sp2 = py4hw.helper.FloatingPointHelper.ieee754_to_sp(ie)
             diff = abs(sp - sp2)/abs(sp)
-            #print('DIFF:', diff)
+            print('DIFF:', diff)
             assert(diff < 1e-7)
             
     def test_fp_2(self):
@@ -57,6 +63,8 @@ class Test_Helper:
         large = math.pow(2,30)
         max_error = 40
 
+        print('big range')
+
         for i in range(1000):
             av = random.uniform(-large, large)            
             rav = fp.ieee754_stored_internally(av)
@@ -64,7 +72,7 @@ class Test_Helper:
             s,e,m = fp.sp_to_ieee754_parts(av)
             iv = fp.ieee754_parts_to_sp(s, e, m)
            
-            #print('initial:',av, 'adjusted:', rav, 'diff:', abs(av-rav))
+            print('initial:',av, 'adjusted:', rav, 'diff:', abs(av-rav))
             
             assert(abs(av-rav) < max_error)
         #print('parts {:01b}-{:08b}-{:023b}'.format( s, e, m))        
@@ -73,4 +81,5 @@ class Test_Helper:
         
             
 if __name__ == '__main__':
-    pytest.main(args=['-s', '-q', 'Test_Helper.py'])
+#    pytest.main(args=['-s', '-q', 'Test_Helper.py'])
+    pytest.main(args=['-s', 'Test_Helper.py'])
