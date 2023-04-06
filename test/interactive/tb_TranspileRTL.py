96ee06aefc0f39fb77708c686c30baf1051322bf
David Castells-Rufas <david.castells@uab.cat>
2023-04-06 13:54:24 +0200

    process handling in the transpiler

--- test/interactive/tb_TranspileRTL.py
+++ test/interactive/tb_TranspileRTL.py
@@ -15,6 +15,21 @@ import textwrap
 import ast
 import webbrowser
 
+class FullAdder(py4hw.Logic):
+    
+    def __init__(self, parent, name, x, y, ci, s, co):
+        super().__init__(parent, name)
+        
+        self.x = self.addIn('x', x)
+        self.y = self.addIn('y', y)
+        self.ci = self.addIn('ci', ci)
+        self.s = self.addOut('s', s)
+        self.co = self.addOut('co', co)
+
+    def propagate(self):
+        self.s.put((self.x.get() ^ self.y.get()) ^ self.ci.get())
+        self.co.put((self.x.get() & self.y.get()) | ( (self.x.get() ^ self.y.get())  & self.ci.get()))
+        
 class TestCircuit(py4hw.Logic):
     def __init__(self, parent, name, a, r):
         super().__init__(parent, name)
@@ -25,7 +40,7 @@ class TestCircuit(py4hw.Logic):
     def clock(self):
         self.r.prepare(self.r.get() + self.a.get())
         
-        
+    
 class FSM(py4hw.Logic):
     def __init__(self, parent, name, a, r):
         super().__init__(parent, name)
@@ -54,28 +69,44 @@ class FSM(py4hw.Logic):
             else:
                 self.count += 1
                 self.r.prepare(10+self.count)
-        
-hw = py4hw.HWSystem()
-a = hw.wire('a', 8)
-r = hw.wire('r', 8)
 
-py4hw.Sequence(hw, 'seq', [1,2,3], a)
-dut = TestCircuit(hw, 'test', a, r)
-#dut = FSM(hw, 'test', a, r)
+if (True):
+    # Test circuit
+    hw = py4hw.HWSystem()
+    a = hw.wire('a', 8)
+    r = hw.wire('r', 8)
+    
+    py4hw.Sequence(hw, 'seq', [1,2,3], a)
+    dut = TestCircuit(hw, 'test', a, r)
+    #dut = FSM(hw, 'test', a, r)
 
+if (False):
+    hw = py4hw.HWSystem()
+    x = hw.wire('x')
+    y = hw.wire('y')
+    ci = hw.wire('ci')
+    s = hw.wire('s')
+    co = hw.wire('co')
+    
+    py4hw.Sequence(hw, 'x', [0,1], x)
+    py4hw.Sequence(hw, 'y', [0,0,1,1], y)
+    py4hw.Sequence(hw, 'ci', [0,0,0,0,1,1,1,1], ci)
+    dut = FullAdder(hw, 'test', x, y, ci, s, co)
+    
 if (True):
     # Low level tests
     module = py2v.getMethod(dut, 'clock')
-    node = py2v.getBody(module)
+    node = py2v.getBody(module, '*')
     
     node = py2v.ReplaceIf().visit(node)        
-    node = py2v.ReplaceWireGets().visit(node)
-    node = py2v.ReplaceWirePrepare().visit(node)
+    node = py2v.ReplaceWireCalls().visit(node)
     node = py2v.ReplaceExpr().visit(node)
     node = py2v.ReplaceBinOp().visit(node)
     node = py2v.ReplaceAttribute().visit(node)
     node = py2v.ReplaceConstant().visit(node)
     node = py2v.ReplaceAssign().visit(node)
+    
+    node = py2v.FlattenOperators().loop_visit(node)
     
     #xml = ast2xml.visit_node(node, ast_name='body')
     xml = ast2xml.visit_node(node)
@@ -83,14 +114,22 @@ if (True):
     with open('c:\\temp\\test.xml', 'wb') as f:
         f.write(ast2xml.renderXml(xml))
 
-if (True):    
+if (False):    
     rtl = py2v.Python2VerilogTranspiler(dut, 'clock')
     str = rtl.transpileRTL()
     
     print('transpilation of clock method')
     print(str)
 
-rtl = py4hw.rtl_generation.VerilogGenerator(dut)
-str = rtl.getVerilog()
+if (False):    
+    rtl = py2v.Python2VerilogTranspiler(dut, 'propagate')
+    str = rtl.transpileRTL()
+    
+    print('transpilation of propagate method')
+    print(str)
 
-print(str)
+if (True):
+    rtl = py4hw.rtl_generation.VerilogGenerator(dut)
+    str = rtl.getVerilog()
+    
+    print(str)
