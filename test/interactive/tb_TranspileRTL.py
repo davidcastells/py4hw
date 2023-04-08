0b018a2deb49b691dc8ebfab70fa975f27cea1d7
David Castells-Rufas <david.castells@uab.cat>
2023-04-08 16:30:52 +0200

    separate methods for sequential and combinational transpilation

--- test/interactive/tb_TranspileRTL.py
+++ test/interactive/tb_TranspileRTL.py
@@ -70,7 +70,41 @@ class FSM(py4hw.Logic):
                 self.count += 1
                 self.r.prepare(10+self.count)
 
-if (True):
+class VGATestPattern(py4hw.Logic):
+    
+    def __init__(self, parent, name, reset, hactive, vactive, r, g, b):
+        super().__init__(parent, name)
+        
+        self.reset = self.addIn('reset', reset)
+        self.hactive = self.addOut('hactive', hactive)
+        self.vactive = self.addOut('vactive', vactive)
+        self.r = self.addOut('r', r)
+        self.g = self.addOut('g', g)
+        self.b = self.addOut('b', b)
+        self.x = 0
+        self.y = 0
+        
+    def clock(self):
+        
+        divx = self.x // 80
+        vr = ((divx >> 0) & 1) * 0xFF
+        vg = ((divx >> 1) & 1) * 0xFF
+        vb = ((divx >> 2) & 1) * 0xFF       
+
+        self.r.prepare(vr)
+        self.g.prepare(vg)
+        self.b.prepare(vb)
+        self.vactive.prepare(1 if self.y < 480 else 0)
+        self.hactive.prepare(1 if self.x < 640 else 0)
+        
+        self.x += 1
+        if (self.x >= 840):
+            self.x = 0
+            self.y += 1
+            if (self.y >= 520):
+                self.y = 0
+
+if (False):
     # Test circuit
     hw = py4hw.HWSystem()
     a = hw.wire('a', 8)
@@ -78,6 +112,21 @@ if (True):
     
     py4hw.Sequence(hw, 'seq', [1,2,3], a)
     dut = TestCircuit(hw, 'test', a, r)
+    #dut = FSM(hw, 'test', a, r)
+
+if (True):
+    # Test circuit
+    hw = py4hw.HWSystem()
+    ha = hw.wire('ha')
+    va = hw.wire('va')
+    r = hw.wire('r',8)
+    g = hw.wire('g',8)
+    b = hw.wire('b',8)
+    
+    reset = hw.wire('reset')
+    py4hw.Constant(hw, 'reset', 0, reset)
+    
+    dut = VGATestPattern(hw, 'vga', reset, ha, va, r, g, b)
     #dut = FSM(hw, 'test', a, r)
 
 if (False):
@@ -94,6 +143,17 @@ if (False):
     dut = FullAdder(hw, 'test', x, y, ci, s, co)
     
 if (True):
+    module = py2v.getMethod(dut, '__init__')
+    node = py2v.getBody(module, '*')
+    
+    node = py2v.ExtractInitializers().visit(node)        
+
+    xml = ast2xml.visit_node(node)
+    
+    with open('c:\\temp\\init.xml', 'wb') as f:
+        f.write(ast2xml.renderXml(xml))
+
+if (True):
     # Low level tests
     module = py2v.getMethod(dut, 'clock')
     node = py2v.getBody(module, '*')
@@ -102,21 +162,24 @@ if (True):
     node = py2v.ReplaceWireCalls().visit(node)
     node = py2v.ReplaceExpr().visit(node)
     node = py2v.ReplaceBinOp().visit(node)
-    node = py2v.ReplaceAttribute().visit(node)
+    ports = {'r':py2v.VerilogWire('r'), 'g':py2v.VerilogWire('g')}
+    variables = {'x':py2v.VerilogVariable('x', 'int'), 'y':py2v.VerilogVariable('y', 'int')}
+    node = py2v.ReplaceWiresAndVariables(ports, variables).visit(node)
     node = py2v.ReplaceConstant().visit(node)
     node = py2v.ReplaceAssign().visit(node)
+    node = py2v.ReplaceIfExp().visit(node)
     
-    node = py2v.FlattenOperators().loop_visit(node)
+    #node = py2v.FlattenOperators().loop_visit(node)
     
     #xml = ast2xml.visit_node(node, ast_name='body')
     xml = ast2xml.visit_node(node)
     
-    with open('c:\\temp\\test.xml', 'wb') as f:
+    with open('c:\\temp\\clock.xml', 'wb') as f:
         f.write(ast2xml.renderXml(xml))
 
 if (False):    
-    rtl = py2v.Python2VerilogTranspiler(dut, 'clock')
-    str = rtl.transpileRTL()
+    rtl = py2v.Python2VerilogTranspiler(dut)
+    str = rtl.transpileSequential()
     
     print('transpilation of clock method')
     print(str)
