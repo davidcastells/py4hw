75bab8cc3e1ee6a26d043cdf7b58a9d52c760437
David Castells-Rufas <david.castells@uab.cat>
2023-05-10 11:36:20 +0200

    clock divider

--- py4hw/logic/clock.py
+++ py4hw/logic/clock.py
@@ -18,3 +18,39 @@ class GatedClock(Logic):
     def propagate(self):
         # this is fake, just to make simulator work
         self.enout.put(self.enin.get())
+        
+        
+class ClockDivider(Logic):
+    def __init__(self, parent, name, freq_in, freq_out, clkout):
+        from py4hw.helper import LogicHelper
+        from py4hw.logic.arithmetic import ModuloCounter
+        from py4hw.logic.storage import TReg
+        import math
+        super().__init__(parent, name)
+            
+        req_freq_out = freq_out
+        hlp = LogicHelper(self)
+        
+        self.addOut('clkout', clkout)
+        
+        n = freq_in / (2*freq_out)
+        
+        qw = int(math.log2(n)) + 1
+        
+        n = int(n)
+        
+        freq_out = freq_in / (2*n)
+        
+        #print('n', n, 'qw', qw, 'freq_out', freq_out)
+        if (freq_out != req_freq_out):
+            print('WARNING: Real Output Frequency:', freq_out, 'required:', req_freq_out)
+        
+        assert(qw > 0)
+        q = self.wire('q', qw)
+        t = self.wire('t')
+        inc = hlp.hw_constant(1,1)
+        reset = hlp.hw_constant(1,0)
+        dut = ModuloCounter(self, 'count', mod=n, inc=inc, reset=reset, q=q, carryout=t)
+    
+        TReg(self, 'clkout', t, e=inc, q=clkout)
+        
