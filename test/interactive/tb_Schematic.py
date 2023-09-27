98fbe32b6fcc1c10bbf0a6a50b687ffa7276b157
David Castells-Rufas <david.castells@uab.cat>
2023-09-27 19:35:01 +0200

    add a mult3w

--- test/interactive/tb_Schematic.py
+++ test/interactive/tb_Schematic.py
@@ -20,7 +20,19 @@ class Mult3(Logic):
         self.addIn('a', a)
         self.addOut('r', r)
         
-        aux = parent.wire('aux', r.getWidth())
+        aux = self.wire('aux', r.getWidth())
+        
+        ShiftLeftConstant(self, 'm2', a, 1, aux)
+        Add(self, "add", a, aux, r)
+        
+class Mult3w(Logic):
+    def __init__(self, parent:Logic, name:str, a:Wire, r:Wire):
+        super().__init__(parent, name)
+
+        self.addIn('very_long_a', a)
+        self.addOut('very_long_r', r)
+        
+        aux = self.wire('aux', r.getWidth())
         
         ShiftLeftConstant(self, 'm2', a, 1, aux)
         Add(self, "add", a, aux, r)
@@ -29,12 +41,18 @@ sys = HWSystem()
 
 a = sys.wire("a", 3)
 r = sys.wire("r", 3)
+r2 = sys.wire("r2", 3)
+b = sys.wire('b')
+c = sys.wire('c')
 p = sys.wire("p", 3)
 
 Constant(sys, 'a', 3, a)
 
 m3 = Mult3(sys, "m3", a, r)
+m3w = Mult3w(sys, "m3w", a, r2)
 Reg(sys, "reg", r, p)
+Bit(sys, 'bit', r, 0, b)
+And2(sys, 'c', b, b, c)
 
 bits = sys.wires('bits', r.getWidth(), 1)
 BitsLSBF(sys, 'bits', r, bits)
@@ -42,5 +60,6 @@ Scope(sys, "p", p)
 #Scope(sys, "r", r)
 
 debug.checkIntegrity(sys)
-Schematic(sys)
+sch = Schematic(sys)
+sch.draw()
 
