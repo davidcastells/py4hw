ea2a293f45b3598262f2338f688c4642f6174ebf
David Castells-Rufas <david.castells@uab.cat>
2024-02-12 16:42:33 +0100

    support custom tkinter GUIs

--- test/interactive/tb_Workbench.py
+++ test/interactive/tb_Workbench.py
@@ -33,7 +33,57 @@ class WrappedAnd(py4hw.Logic):
         
         py4hw.And2(self, 'and', a, b, r)
 
+class RandomValue(py4hw.Logic):
+    def __init__(self, parent, name, r, mean, stddev):
+        super().__init__(parent, name)
+        
+        self.r = self.addOut('r', r)
+        
+        self.mean = mean
+        self.stddev = stddev
+        
+    def clock(self):
+        import numpy as np
+        v = int(np.random.normal(self.mean, self.stddev))
+        self.r.prepare(v)
+        
+    def update_mean(self, v):
+        self.mean = float(v)
+        self.label1.config(text="Mean: {}".format(self.mean))
+        
+    def update_stddev(self, v):
+        self.stddev = float(v)
+        self.label2.config(text="Stddev: {}".format(self.stddev))
+        
+    def tkinter_gui(self, parent):
+        from tkinter import ttk
+        import tkinter as tk
+        frame = ttk.Frame(parent, padding="10")
+        frame.grid(column=2, row=2, sticky=(tk.W, tk.E, tk.N, tk.S))
 
+        maxv = (2 ** self.r.getWidth()) -1
+        
+        # Create sliders and labels
+        self.label1 = ttk.Label(frame, text="Mean: {}".format(self.mean))
+        self.label2 = ttk.Label(frame, text="Stddev: {}".format(self.stddev))
+
+        slider1 = ttk.Scale(frame, from_=0, to=maxv, orient="horizontal", command=self.update_mean)
+        slider1.set(self.mean)
+        slider2 = ttk.Scale(frame, from_=0, to=maxv, orient="horizontal", command=self.update_stddev)
+        slider2.set(self.stddev)
+
+        # Grid layout for sliders and labels
+        slider1.grid(column=0, row=0, columnspan=2, pady=10)
+        slider2.grid(column=0, row=1, columnspan=2, pady=10)
+        self.label1.grid(column=1, row=0, padx=5)
+        self.label2.grid(column=1, row=1, padx=5)
+        
+        # Configure column weights to make the sliders and labels expandable
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=1)
+        
+        return frame
+    
 
 sys = HWSystem()
 
@@ -41,6 +91,7 @@ a = Wire(sys, "a", 4)
 b = Wire(sys, "b", 4)
 c = Wire(sys, "c", 4)
 d = Wire(sys, "d", 4)
+e = Wire(sys, "e", 4)
 
 r = Wire(sys, "r", 4)
 
@@ -50,5 +101,6 @@ SerialAdder(sys, "sa", a, r)
 #And2(sys, 'and', a, b, c)
 WrappedAnd(sys, 'and', a, a, c)
 WrappedAnd(sys, 'and2', a, c, d)
+RandomValue(sys, 'random', e, 1<<3, 1<<3)
 
 py4hw.gui.Workbench(sys)
