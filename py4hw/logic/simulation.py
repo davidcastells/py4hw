b97354bc29c5a055097f6405db3a5759504573a1
David Castells-Rufas <david.castells@uab.cat>
2024-10-02 12:07:26 +0200

    random value

--- py4hw/logic/simulation.py
+++ py4hw/logic/simulation.py
@@ -623,3 +623,62 @@ class Sequence(Logic):
     def clock(self):
         self.r.prepare(self.values[self.i])
         self.i = ( self.i +1 ) % self.n
+
+
+class RandomValue(Logic):
+    def __init__(self, parent, name, r, mean, stddev):
+        super().__init__(parent, name)
+        
+        self.r = self.addOut('r', r)
+        
+        self.mean = mean
+        self.stddev = stddev
+        self.state = '{} <- {}+-{}'.format(self.r.get(), self.mean, self.stddev)
+        
+    def clock(self):
+        import numpy as np
+        v = int(np.random.normal(self.mean, self.stddev))
+        self.r.prepare(v)
+        self.state = '{} <- {}+-{}'.format(v, self.mean, self.stddev)
+        
+    def update_mean(self, v):
+        self.mean = float(v)
+        self.label1.config(text="Mean: {}".format(self.mean))
+        
+    def update_stddev(self, v):
+        self.stddev = float(v)
+        self.label2.config(text="Stddev: {}".format(self.stddev))
+        
+    def tkinter_gui(self, parent):
+        from tkinter import ttk
+        import tkinter as tk
+        frame = ttk.Frame(parent, padding="10")
+        frame.grid(column=2, row=2, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+        maxv = (2 ** self.r.getWidth()) -1
+        
+        print('maxv : ', maxv)
+        
+        # Create sliders and labels
+        
+        self.label1 = ttk.Label(frame, text="Mean: {}".format(self.mean))
+        self.label2 = ttk.Label(frame, text="Stddev: {}".format(self.stddev))
+
+        slider1 = ttk.Scale(frame, from_=0, to=maxv, orient="horizontal", command=self.update_mean)
+        slider1.set(self.mean)
+        slider2 = ttk.Scale(frame, from_=0, to=maxv, orient="horizontal", command=self.update_stddev)
+        slider2.set(self.stddev)
+        
+        
+
+        # Grid layout for sliders and labels
+        slider1.grid(column=0, row=0,  pady=10)
+        slider2.grid(column=0, row=1,  pady=10)
+        self.label1.grid(column=1, row=0, padx=5)
+        self.label2.grid(column=1, row=1, padx=5)
+        
+        # Configure column weights to make the sliders and labels expandable
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=1)
+        
+        return frame
