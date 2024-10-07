8438ac83d7e99f295da3b5168c59a4b7ed9afbc8
David Castells-Rufas <david.castells@uab.cat>
2024-10-07 14:13:27 +0200

    py4hw imports

--- py4hw/logic/protocol/uart/clock.py
+++ py4hw/logic/protocol/uart/clock.py
@@ -40,6 +40,10 @@ class ClockGenerationAndRecovery(Logic):
     def __init__(self, parent, name, rx, desync, tx_clk_pulse, rx_sample, sysFreq, uartFreq):
         super().__init__(parent, name)
         
+        from py4hw.logic.clock import ClockDivider
+        from py4hw.logic.clock import EdgeDetector
+        from py4hw.logic.bitwise import And2
+        
         self.addIn('rx', rx)
         self.addIn('desync', desync)
         self.addOut('tx_clk_pulse', tx_clk_pulse)
@@ -53,12 +57,12 @@ class ClockGenerationAndRecovery(Logic):
         sync = self.wire('sync')
         active = self.wire('active')
 
-        py4hw.ClockDivider(self, 'uart_clk', sysFreq, uartFreq, uartClk)
+        ClockDivider(self, 'uart_clk', sysFreq, uartFreq, uartClk)
         ClockSyncFSM(self, 'clk_sync', start, desync, sync, active)
-        py4hw.ClockDivider(self, 'sync_uart_clk', sysFreq, uartFreq, syncUartClk, reset=start)
+        ClockDivider(self, 'sync_uart_clk', sysFreq, uartFreq, syncUartClk, reset=start)
         
-        py4hw.EdgeDetector(self, 'pos_edge', uartClk, tx_clk_pulse, 'pos')
-        py4hw.EdgeDetector(self, 'start', rx, start, 'pos')
-        py4hw.EdgeDetector(self, 'sample', syncUartClk, pre_rx_sample, 'neg')
+        EdgeDetector(self, 'pos_edge', uartClk, tx_clk_pulse, 'pos')
+        EdgeDetector(self, 'start', rx, start, 'pos')
+        EdgeDetector(self, 'sample', syncUartClk, pre_rx_sample, 'neg')
         
-        py4hw.And2(self, 'rx_sample', pre_rx_sample, active, rx_sample)
+        And2(self, 'rx_sample', pre_rx_sample, active, rx_sample)
