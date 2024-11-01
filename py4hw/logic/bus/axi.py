3581d91fb33edf576bc13755374a77d8df108887
David Castells-Rufas <david.castells@uab.cat>
2024-11-01 06:55:17 +0100

    awsize

--- py4hw/logic/bus/axi.py
+++ py4hw/logic/bus/axi.py
@@ -17,13 +17,18 @@ class AXI4Interface(Interface):
         self.awready = self.addSinkToSource('awready', 1)
         self.awaddr = self.addSourceToSink('awaddr', aw)
         self.awlen = self.addSourceToSink('awlen', 8)
+        self.awsize = self.addSourceToSink('awsize', 3) # data transfer size in bytes = 2^awsize
+        
         self.wvalid = self.addSourceToSink('wvalid', 1)
         self.wready = self.addSinkToSource('wready', 1)
         self.wdata = self.addSourceToSink('wdata', dw)
         self.wstrb = self.addSourceToSink('wstrb', dw//8)
         self.wlast = self.addSourceToSink('wlast', 1)
+        
         self.bvalid = self.addSinkToSource('bvalid', 1)
         self.bready = self.addSourceToSink('bready', 1)
+        self.rresp = self.addSinkToSource('rresp', 2)
+        
         self.arvalid = self.addSourceToSink('arvalid', 1)
         self.arready = self.addSinkToSource('arready', 1)
         self.araddr = self.addSourceToSink('araddr', aw)
@@ -42,17 +47,21 @@ class AXI4LiteInterface(Interface):
         self.awvalid = self.addSourceToSink('awvalid', 1)
         self.awready = self.addSinkToSource('awready', 1)
         self.awaddr = self.addSourceToSink('awaddr', aw)
+        
         self.wvalid = self.addSourceToSink('wvalid', 1)
         self.wready = self.addSinkToSource('wready', 1)
         self.wdata = self.addSourceToSink('wdata', dw)
         self.wstrb = self.addSourceToSink('wstrb', dw//8)
+        
         self.arvalid = self.addSourceToSink('arvalid', 1)
         self.arready = self.addSinkToSource('arready', 1)
         self.araddr = self.addSourceToSink('araddr', aw)
+        
         self.rvalid = self.addSinkToSource('rvalid', 1)
         self.rready = self.addSourceToSink('rready', 1)
         self.rdata = self.addSinkToSource('rdata', dw)
         self.rresp = self.addSinkToSource('rresp', 2)
+        
         self.bvalid = self.addSinkToSource('bvalid', 1)
         self.bready = self.addSourceToSink('bready', 1)
         self.bresp = self.addSinkToSource('bresp', 2)
