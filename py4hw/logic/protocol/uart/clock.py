c1e6dadecacc0b8bac7713d06baf5097529c8b30
David Castells-Rufas <david.castells@uab.cat>
2025-07-06 13:29:26 +0200

    BUGFIX: rx clock should only be resynchronized after the start bit

--- py4hw/logic/protocol/uart/clock.py
+++ py4hw/logic/protocol/uart/clock.py
@@ -39,12 +39,22 @@ class ClockSyncFSM(Logic):
                 
 
 class ClockGenerationAndRecovery(Logic):
+    ''' 
+    The transmission clock (uartClk) is generated as a divisor of the original clock.
+    However this signal is never used as a clock to any circuit, it is only used to detect positive edges that are used for transmission.
+    Transmission clock is easy because it has not to be synchronized with anyone.
+    
+    The reception clock (syncUartClk) is generated similarly, with the difference that has a reset signal.
+    The reset should occur when a negative rx pulse is seen and we are not in sync
+    '''
+    
     def __init__(self, parent, name, rx, desync, tx_clk_pulse, rx_sample, sysFreq, uartFreq):
         super().__init__(parent, name)
         
         from py4hw.logic.clock import ClockDivider
         from py4hw.logic.clock import EdgeDetector
         from py4hw.logic.bitwise import And2
+        from py4hw.logic.bitwise import Not
         
         self.addIn('rx', rx)
         self.addIn('desync', desync)
@@ -55,16 +65,21 @@ class ClockGenerationAndRecovery(Logic):
         syncUartClk = self.wire('sync_uart_clk')
         pre_rx_sample = self.wire('pre_rx_sample')
         
+        rx_neg = self.wire('rx_neg')
         start = self.wire('start')
         sync = self.wire('sync')
+        
         active = self.wire('active')
+        nactive = self.wire('nactive')
 
         ClockDivider(self, 'uart_clk', sysFreq, uartFreq, uartClk)
         ClockSyncFSM(self, 'clk_sync', start, desync, sync, active)
         ClockDivider(self, 'sync_uart_clk', sysFreq, uartFreq, syncUartClk, reset=start)
         
+        Not(self, 'nactive', active, nactive)
         EdgeDetector(self, 'pos_edge', uartClk, tx_clk_pulse, 'pos')
-        EdgeDetector(self, 'start', rx, start, 'neg')
+        EdgeDetector(self, 'rx_neg', rx, rx_neg, 'neg')
+        And2(self, 'start', rx_neg, nactive, start)
         
         # the RX synchonized clock is reset with the neg edge of RX and starts with value 0, then sample must occur at positive edge
         EdgeDetector(self, 'sample', syncUartClk, pre_rx_sample, 'pos') 
