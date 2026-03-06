2684a29a8c857e55fa9f3cc8eb3f9445e4c78fc8
David Castells-Rufas <david.castells@uab.cat>
2026-03-06 21:58:49 +0100

    comments

--- py4hw/transpilation/__init__.py
+++ py4hw/transpilation/__init__.py
@@ -6,3 +6,14 @@ Created on Sun Apr  2 14:23:01 2023
 """
 
 from .. import *
+
+
+# From                              |  To                               | Class                                            | Instance / Class
+# Structural py4hw                  | Verilog                           | VerilogGenerator                                 | Instance
+# Behavioural combinational py4hw   | Verilog                           | VerilogGenerator -> python2verilog_transpiler    | Instance
+# Behavioural sequential py4hw      | Verilog                           | VerilogGenerator -> python2verilog_transpiler    | Instance
+# Algorithmic Timed py4hw           | Verilog                           | ?
+# Behavioural combinational py4hw   | Structuural py4hw                 | ?
+# Behavioural combinational py4hw   | Structuural py4hw                 | ? 
+# Algorithmic Timed py4hw           | Behavioural combinational py4hw   | Py4hwGenerator
+ 
