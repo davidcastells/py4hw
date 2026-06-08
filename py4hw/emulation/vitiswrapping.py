e3a0ebeae327f340c1c984c2811e3ab66a4e0112
David Castells-Rufas <david.castells@uab.cat>
2026-06-08 18:47:46 +0200

    emulation

--- py4hw/emulation/vitiswrapping.py
+++ py4hw/emulation/vitiswrapping.py
@@ -213,7 +213,8 @@ class HILPlatform:
 	
         print('Generating', verilog_file)
         
-        # 2. Generate TCL
+        # 2. Generate TCLs
+        generate_create_project_tcl(kernel, [verilog_file, dut_file], self.projectDir)
         generate_package_tcl(kernel, [verilog_file, dut_file], self.projectDir)
         
         # 3. Run Vivado
@@ -317,11 +318,23 @@ def createHILVitis(platform, dut, projectDir):
     fake_outs = []
         
     ap_start = platform.wire('ap_start')
-    ap_reset = platform.wire('ap_reset')
-    reset = platform.wire('reset')
+    ap_reset = platform.wire('ap_rst_n')
+    
+    ap_idle = platform.wire('ap_idle')
+    ap_done = platform.wire('ap_done')
+    ap_ready = platform.wire('ap_ready')
+    
+                
+    #reset = platform.wire('reset')
     rtl_kernel.addIn('ap_start', ap_start)
-    rtl_kernel.addIn('ap_reset', ap_reset)
-    rtl_kernel.addIn('reset', reset)
+    rtl_kernel.addIn('ap_rst_n', ap_rst_n)
+    rtl_kernel.addOut('ap_idle', ap_idle)
+    rtl_kernel.addOut('ap_done', ap_done)
+    rtl_kernel.addOut('ap_ready', ap_ready)        
+        
+    #rtl_kernel.addIn('reset', reset)
+    
+    py4hw.Not(rtl_kernel, 'ap_reset', ap_rst_n, ap_reset)
     
     stream_ins = []
         
@@ -636,9 +649,13 @@ extern "C" void krnl_writer_generic(
     
     
 def generate_rtl_kernel(output_path):
-    tcl_path = os.path.join(output_path, 'package_rtl_kernel.tcl')
+    tcl_path = os.path.join(output_path, 'create_project.tcl')
     cmd = f'vivado -mode batch -source {tcl_path}'
     os.system(cmd)
+
+    tcl_path = os.path.join(output_path, 'package_rtl_kernel.tcl')
+    cmd = f'vivado -mode batch -source {tcl_path}'
+    #os.system(cmd)
     
 '''
 #!/usr/bin/env python3
@@ -1359,6 +1376,26 @@ def collect_rtl_files(cfg: BuildConfig) -> list[Path]:
 
     return files
 
+def generate_create_project_tcl(dur, dut_files, output_dir):
+    rtl_kernel_name = 'rtl_kernel'
+    
+    xci_path = f'{output_dir}/{rtl_kernel_name}.srcs/sources_1/ip/{rtl_kernel_name}/{rtl_kernel_name}.xci'
+    tcl = f'''
+    create_project {rtl_kernel_name} {output_dir} -part xcu55c-fsvh2892-2L-e
+    create_ip -name rtl_kernel_wizard -vendor xilinx.com -library ip -version 1.0 -module_name {rtl_kernel_name}
+    set_property -dict [list \
+       CONFIG.KERNEL_VENDOR {{xilinx.com}} \
+       CONFIG.NUM_AXIS {{2}} \
+       CONFIG.NUM_INPUT_ARGS {{0}} \
+       CONFIG.NUM_M_AXI {{0}} \
+       CONFIG.NUM_RESETS {{1}} \
+       ] [get_ips {rtl_kernel_name}]
+    generate_target {{instantiation_template}} [get_files {xci_path}]
+    update_compile_order -fileset sources_1
+    open_example_project -dir {output_dir} [get_ips  {rtl_kernel_name}]
+    '''
+    write_text(os.path.join(output_dir, 'create_project.tcl'), tcl)
+    print("[OK] create_project.tcl generat")
 
 def generate_package_tcl(dut, dut_files, output_dir) -> None:
     rtl_files = []
@@ -1371,60 +1408,224 @@ def generate_package_tcl(dut, dut_files, output_dir) -> None:
     rtl_kernel_name = 'rtl_kernel'
     rtl_xo_path = os.path.join(output_dir, rtl_kernel_name+'.xo')
 
-    tcl = f"""# Auto-generated package RTL TCL
+    tcl = f'''
+# This is a generated file. Use and modify at your own risk.
+################################################################################
 
-set kernel_name "{rtl_kernel_name}"
-set top_module  "{rtl_kernel_name}"
+set kernel_name    "{rtl_kernel_name}"
+set kernel_vendor  "xilinx.com"
+set kernel_library "kernel"
 
-set build_dir [file normalize "{output_dir}"]
-set xo_path   [file normalize "{rtl_xo_path}"]
-set proj_dir  [file normalize "$build_dir/vivado_$kernel_name"]
-set ip_dir    [file normalize "$build_dir/ip_$kernel_name"]
+##############################################################################
 
-file mkdir $build_dir
-file delete -force $proj_dir
-file delete -force $ip_dir
-file delete -force $xo_path
+proc edit_core {{core}} {{
+  set bif      [::ipx::get_bus_interfaces -of $core  "fontread_axi_m"] 
+  set bifparam [::ipx::add_bus_parameter -quiet "MAX_BURST_LENGTH" $bif]
+  set_property value        256          $bifparam
+  set_property value_source constant     $bifparam
+  set bifparam [::ipx::add_bus_parameter -quiet "NUM_READ_OUTSTANDING" $bif]
+  set_property value        32           $bifparam
+  set_property value_source constant     $bifparam
+  set bifparam [::ipx::add_bus_parameter -quiet "NUM_WRITE_OUTSTANDING" $bif]
+  set_property value        32           $bifparam
+  set_property value_source constant     $bifparam
 
-# ATENCI:
-# Aquesta part est posada per U55C.
-# Si es fs servir una altra placa, s'hauria de canviar el part.
-create_project -force $kernel_name $proj_dir -part xcu55c-fsvh2892-2L-e
+  ::ipx::associate_bus_interfaces -busif "fontread_axi_m" -clock "ap_clk" $core
+  ::ipx::associate_bus_interfaces -busif "dataout_axis_m" -clock "ap_clk" $core
+  set axis_bif      [::ipx::get_bus_interfaces -of $core  "dataout_axis_m"] 
+  set bifparam [::ipx::add_bus_parameter -quiet "TDATA_NUM_BYTES" $axis_bif]
+  set_property value        8   $bifparam
+  set_property value_source constant     $bifparam
+  set bifparam [::ipx::add_bus_parameter -quiet "TUSER_WIDTH" $axis_bif]
+  set_property value        0   $bifparam
+  set_property value_source constant     $bifparam
+  set bifparam [::ipx::add_bus_parameter -quiet "TID_WIDTH" $axis_bif]
+  set_property value        0   $bifparam
+  set_property value_source constant     $bifparam
+  set bifparam [::ipx::add_bus_parameter -quiet "TDEST_WIDTH" $axis_bif]
+  set_property value        0   $bifparam
+  set_property value_source constant     $bifparam
+  set bifparam [::ipx::add_bus_parameter -quiet "HAS_TREADY" $axis_bif]
+  set_property value        1   $bifparam
+  set_property value_source constant     $bifparam
+  set bifparam [::ipx::add_bus_parameter -quiet "HAS_TSTRB" $axis_bif]
+  set_property value        0   $bifparam
+  set_property value_source constant     $bifparam
+  set bifparam [::ipx::add_bus_parameter -quiet "HAS_TKEEP" $axis_bif]
+  set_property value        1   $bifparam
+  set_property value_source constant     $bifparam
+  set bifparam [::ipx::add_bus_parameter -quiet "HAS_TLAST" $axis_bif]
+  set_property value        1   $bifparam
+  set_property value_source constant     $bifparam
+  ::ipx::associate_bus_interfaces -busif "s_axi_control" -clock "ap_clk" $core
 
-{add_files_lines}
+  # Specify the freq_hz parameter 
+  set clkbif      [::ipx::get_bus_interfaces -of $core "ap_clk"]
+  set clkbifparam [::ipx::add_bus_parameter -quiet "FREQ_HZ" $clkbif]
+  # Set desired frequency                   
+  set_property value 250000000 $clkbifparam
+  # set value_resolve_type 'user' if the frequency can vary. 
+  set_property value_resolve_type user $clkbifparam
+  # set value_resolve_type 'immediate' if the frequency cannot change. 
+  # set_property value_resolve_type immediate $clkbifparam
+  set mem_map    [::ipx::add_memory_map -quiet "s_axi_control" $core]
+  set addr_block [::ipx::add_address_block -quiet "reg0" $mem_map]
 
-set_property top $top_module [current_fileset]
-update_compile_order -fileset sources_1
+  set reg      [::ipx::add_register "CTRL" $addr_block]
+  set_property description    "Control signals"    $reg
+  set_property address_offset 0x000 $reg
+  set_property size           32    $reg
+  set field [ipx::add_field AP_START $reg]
+    set_property ACCESS {{read-write}} $field
+    set_property BIT_OFFSET {{0}} $field
+    set_property BIT_WIDTH {{1}} $field
+    set_property DESCRIPTION {{Control signal Register for 'ap_start'.}} $field
+    set_property MODIFIED_WRITE_VALUE {{modify}} $field
+  set field [ipx::add_field AP_DONE $reg]
+    set_property ACCESS {{read-only}} $field
+    set_property BIT_OFFSET {{1}} $field
+    set_property BIT_WIDTH {{1}} $field
+    set_property DESCRIPTION {{Control signal Register for 'ap_done'.}} $field
+    set_property READ_ACTION {{modify}} $field
+  set field [ipx::add_field AP_IDLE $reg]
+    set_property ACCESS {{read-only}} $field
+    set_property BIT_OFFSET {{2}} $field
+    set_property BIT_WIDTH {{1}} $field
+    set_property DESCRIPTION {{Control signal Register for 'ap_idle'.}} $field
+    set_property READ_ACTION {{modify}} $field
+  set field [ipx::add_field AP_READY $reg]
+    set_property ACCESS {{read-only}} $field
+    set_property BIT_OFFSET {{3}} $field
+    set_property BIT_WIDTH {{1}} $field
+    set_property DESCRIPTION {{Control signal Register for 'ap_ready'.}} $field
+    set_property READ_ACTION {{modify}} $field
+  set field [ipx::add_field RESERVED_1 $reg]
+    set_property ACCESS {{read-only}} $field
+    set_property BIT_OFFSET {{4}} $field
+    set_property BIT_WIDTH {{3}} $field
+    set_property DESCRIPTION {{Reserved.  0s on read.}} $field
+    set_property READ_ACTION {{modify}} $field
+  set field [ipx::add_field AUTO_RESTART $reg]
+    set_property ACCESS {{read-write}} $field
+    set_property BIT_OFFSET {{7}} $field
+    set_property BIT_WIDTH {{1}} $field
+    set_property DESCRIPTION {{Control signal Register for 'auto_restart'.}} $field
+    set_property MODIFIED_WRITE_VALUE {{modify}} $field
+  set field [ipx::add_field RESERVED_2 $reg]
+    set_property ACCESS {{read-only}} $field
+    set_property BIT_OFFSET {{8}} $field
+    set_property BIT_WIDTH {{24}} $field
+    set_property DESCRIPTION {{Reserved.  0s on read.}} $field
+    set_property READ_ACTION {{modify}} $field
 
-# Comprovaci RTL
-synth_design -rtl -top $top_module
+  set reg      [::ipx::add_register "GIER" $addr_block]
+  set_property description    "Global Interrupt Enable Register"    $reg
+  set_property address_offset 0x004 $reg
+  set_property size           32    $reg
 
-# Empaquetar com a IP
-ipx::package_project \\
-    -root_dir $ip_dir \\
-    -vendor user.org \\
-    -library user \\
-    -taxonomy /UserIP \\
-    -import_files \\
-    -set_current true
+  set reg      [::ipx::add_register "IP_IER" $addr_block]
+  set_property description    "IP Interrupt Enable Register"    $reg
+  set_property address_offset 0x008 $reg
+  set_property size           32    $reg
 
-set core [ipx::current_core]
-set_property sdx_kernel true $core
-set_property sdx_kernel_type rtl $core
-set_property vitis_drc {{ctrl_protocol ap_ctrl_hs}} $core
+  set reg      [::ipx::add_register "IP_ISR" $addr_block]
+  set_property description    "IP Interrupt Status Register"    $reg
+  set_property address_offset 0x00C $reg
+  set_property size           32    $reg
 
-ipx::save_core $core
-close_project
+  set reg      [::ipx::add_register -quiet "work_mode" $addr_block]
+  set_property address_offset 0x010 $reg
+  set_property size           [expr {4*8}]   $reg
 
-# Generar el fitxer .xo del kernel RTL
-package_xo -force \\
-    -xo_path $xo_path \\
-    -kernel_name $kernel_name \\
-    -ip_directory $ip_dir \\
-    -ctrl_protocol ap_ctrl_hs
+  set reg      [::ipx::add_register -quiet "cs_count" $addr_block]
+  set_property address_offset 0x018 $reg
+  set_property size           [expr {4*8}]   $reg
 
-puts "XO RTL generat a: $xo_path"
-"""
+  set reg      [::ipx::add_register -quiet "time_format" $addr_block]
+  set_property address_offset 0x020 $reg
+  set_property size           [expr {4*8}]   $reg
+
+  set reg      [::ipx::add_register -quiet "time_set_val" $addr_block]
+  set_property address_offset 0x028 $reg
+  set_property size           [expr {4*8}]   $reg
+
+  set reg      [::ipx::add_register -quiet "time_set_en" $addr_block]
+  set_property address_offset 0x030 $reg
+  set_property size           [expr {4*8}]   $reg
+
+  set reg      [::ipx::add_register -quiet "read_addr" $addr_block]
+  set_property address_offset 0x038 $reg
+  set_property size           [expr {8*8}]   $reg
+  set regparam [::ipx::add_register_parameter -quiet {{ASSOCIATED_BUSIF}} $reg] 
+  set_property value fontread_axi_m $regparam 
+
+  set_property slave_memory_map_ref "s_axi_control" [::ipx::get_bus_interfaces -of $core "s_axi_control"]
+
+  set_property xpm_libraries {{XPM_CDC XPM_MEMORY XPM_FIFO}} $core
+  set_property sdx_kernel true $core
+  set_property sdx_kernel_type rtl $core
+}}
+
+##############################################################################
+
+proc package_project {{path_to_packaged kernel_vendor kernel_library kernel_name}} {{
+  set core [::ipx::package_project -root_dir $path_to_packaged -vendor $kernel_vendor -library $kernel_library -taxonomy "/KernelIP" -import_files -set_current false ]
+  foreach user_parameter [list C_S_AXI_CONTROL_ADDR_WIDTH C_S_AXI_CONTROL_DATA_WIDTH C_FONTREAD_AXI_M_ADDR_WIDTH C_FONTREAD_AXI_M_DATA_WIDTH C_DATAOUT_AXIS_M_TDATA_WIDTH] {{
+    ::ipx::remove_user_parameter $user_parameter $core
+  }}
+  ::ipx::create_xgui_files $core
+  set_property supported_families {{ }} $core
+  set_property auto_family_support_level level_2 $core
+  set_property used_in {{out_of_context implementation synthesis}} [::ipx::get_files -type xdc -of_objects [::ipx::get_file_groups "xilinx_anylanguagesynthesis" -of_objects $core] *_ooc.xdc]
+  edit_core $core
+  ::ipx::update_checksums $core
+  ::ipx::check_integrity -kernel $core
+  ::ipx::check_integrity -xrt $core
+  ::ipx::save_core $core
+  ::ipx::unload_core $core
+  unset core
+}}
+
+##############################################################################
+
+proc package_project_dcp {{path_to_dcp path_to_packaged kernel_vendor kernel_library kernel_name}} {{
+  set core [::ipx::package_checkpoint -dcp_file $path_to_dcp -root_dir $path_to_packaged -vendor $kernel_vendor -library $kernel_library -name $kernel_name -taxonomy "/KernelIP" -force]
+  edit_core $core
+  ::ipx::update_checksums $core
+  ::ipx::check_integrity -kernel $core
+  ::ipx::check_integrity -xrt $core
+  ::ipx::save_core $core
+  ::ipx::unload_core $core
+  unset core
+}}
+
+##############################################################################
+
+proc package_project_dcp_and_xdc {{path_to_dcp path_to_xdc path_to_packaged kernel_vendor kernel_library kernel_name}} {{
+  set core [::ipx::package_checkpoint -dcp_file $path_to_dcp -root_dir $path_to_packaged -vendor $kernel_vendor -library $kernel_library -name $kernel_name -taxonomy "/KernelIP" -force]
+  edit_core $core
+  set rel_path_to_xdc [file join "impl" [file tail $path_to_xdc]]
+  set abs_path_to_xdc [file join $path_to_packaged $rel_path_to_xdc]
+  file mkdir [file dirname $abs_path_to_xdc]
+  file copy $path_to_xdc $abs_path_to_xdc
+  set xdcfile [::ipx::add_file $rel_path_to_xdc [::ipx::add_file_group "xilinx_implementation" $core]]
+  set_property type "xdc" $xdcfile
+  set_property used_in [list "implementation"] $xdcfile
+  ::ipx::update_checksums $core
+  ::ipx::check_integrity -kernel $core
+  ::ipx::check_integrity -xrt $core
+  ::ipx::save_core $core
+  ::ipx::unload_core $core
+  unset core
+}}
+
+##############################################################################
+
+#Now run 
+package_project {output_dir} xilinx.com kernel {rtl_kernel_name} 
+package_xo  -xo_path {rtl_xo_path} -kernel_name {rtl_kernel_name} -ip_directory {output_dir} -kernel_xml kernel.xml
+
+'''
 
     write_text(os.path.join(output_dir, 'package_rtl_kernel.tcl'), tcl)
     print("[OK] package_rtl_kernel.tcl generat")
