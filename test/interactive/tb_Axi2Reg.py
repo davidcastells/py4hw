8005ad4e49e9de6e14ce475422b96d5d1af422dc
David Castells-Rufas <david.castells@uab.cat>
2026-07-12 21:07:50 +0200

    ap_done

--- test/interactive/tb_Axi2Reg.py
+++ test/interactive/tb_Axi2Reg.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Apr 17 09:17:17 2026
 
-@author: 2016570
+@author: dcr
 """
 import py4hw
 from py4hw.logic.bus.axi import *
@@ -14,39 +14,24 @@ sys = py4hw.HWSystem()
 
 ap_start = sys.wire("ap_start", 1)
 ap_reset = sys.wire("ap_reset", 1)
+ap_done = sys.wire("ap_done")
 q = sys.wire("q", 64)
 loaded = sys.wire("loaded", 1)
+active = sys.wire("active", 1)
 
 # Create AXI stream interface
 stream = AXI4StreamInterface(sys, "stream", dw=64)
 
 # Instantiate DUT
-dut = Axi2Reg(sys, "axi2reg",  ap_start, ap_reset, stream, q, loaded)
+dut = Axi2Reg(sys, "axi2reg",  ap_start, ap_reset, ap_done,  stream, q, loaded, active)
 
-ap_reset.put(1)
-sys.getSimulator().clk(1)
+py4hw.Sequence(sys, 'ap_start', [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], ap_start)
+py4hw.Sequence(sys, 'ap_reset', [1, 0], ap_reset, once=True)
+py4hw.Sequence(sys, 'ap_done', [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0], ap_done)
+py4hw.Sequence(sys, 'tvalid', [0, 0, 1, 0, 0, 0, 0], stream.tvalid)
+py4hw.Sequence(sys, 'tdata', [0, 1, 2, 3, 4], stream.tdata)
 
-ap_reset.put(0)
-sys.getSimulator().clk(1)
-                       
-assert loaded.get() == 0, 'loaded should be zero'
+wvf = py4hw.Waveform(sys, 'wvf', [ap_start, ap_reset, stream.tvalid, stream.tready, stream.tdata, q, loaded, active])
 
-# Test 1: Capture on valid data
-test_data = 0x123456789ABCDEF0
-stream.tvalid.put(1)
-stream.tdata.put(test_data)
-
-# Give time for signals to propagate
-sys.getSimulator().clk(2)
-
-stream.tvalid.put(0)
-sys.getSimulator().clk(1)
-
-# Should capture on first cycle with valid data
-assert q.get() == test_data, 'unexpected test_data'
-assert loaded.get() == 1, 'loaded should be one'
-
-ap_reset.put(1)
-sys.getSimulator().clk(1)
 
 py4hw.gui.Workbench(sys)
