47efb0a17e792f590dfc31e4812faffe1d864d3a
David Castells-Rufas <david.castells@uab.cat>
2026-07-30 19:41:01 +0200

    pynq z2

--- py4hw/external/platforms/tul.py
+++ py4hw/external/platforms/tul.py
@@ -2,6 +2,20 @@
 """
 Created on Wed Jul 29 09:08:37 2026
 
-@author: 2016570
+@author: dcr
 """
 
+import py4hw
+from edalize import edatool
+import os
+
+
+class Pynq_Z2(py4hw.HWSystem):
+    def __init__(self):
+        super().__init__(name='Pynq-Z2')
+        
+        clk125 = self.wire('sysclk')
+        
+        clockDriver = py4hw.ClockDriver('sysclk', 125E6, 0, wire=clk125)
+        
+        self.clockDriver = clockDriver
