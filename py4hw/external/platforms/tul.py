6072d43be69bda4ffb304d1b7246d0302bfce9c9
David Castells-Rufas <david.castells@uab.cat>
2026-09-16 06:49:40 +0530

    raspberry inputs and outputs

--- py4hw/external/platforms/tul.py
+++ py4hw/external/platforms/tul.py
@@ -58,6 +58,63 @@ class Pynq_Z2(py4hw.HWSystem):
         self.addOut('led', key)
 
         return key
+
+    def getRaspberryPIInput(self, i):
+        
+        ii = self.wire(f'ii{i}')
+        o = self.wire(f'o{i}')
+        
+        wbidir = py4hw.FakeWire(f'rpio_{i}_r')
+        self.addInOut(f'rpio_{i}_r', self.wire(f'w_rpio_{i}_r'))
+                
+        from py4hw.external.xilinx.io import TristateBuffer
+        
+        t = self.wire(f't{i}')
+        py4hw.Constant(self, f't{i}', 1, t)
+        TristateBuffer(self, f'tbuf{i}', ii, t, o, wbidir)
+            
+        return o
+
+    def getRaspberryPIOutput(self, i):
+        
+        oe = self.wire(f'oe{i}')
+        ii = self.wire(f'ii{i}')
+        o = self.wire(f'o{i}')
+        
+        wbidir = py4hw.FakeWire(f'rpio_{i}_r')
+        self.addInOut(f'rpio_{i}_r', self.wire(f'w_rpio_{i}_r'))
+                
+        from py4hw.external.xilinx.io import TristateBuffer
+        
+        t = self.wire(f't{i}')
+        py4hw.Constant(self, f't{i}', 0, t)
+        TristateBuffer(self, f'tbuf{i}', ii, t, o, wbidir)
+            
+        return ii
+
+    
+    # def getRaspberryPIIOs(self):
+        
+    #     oes = self.wires('oes', 20, 1)
+    #     iis = self.wires('iis', 20, 1)
+    #     os = self.wires('os', 20, 1)
+        
+    #     wbidirs = []
+        
+    #     for i in range(20):
+    #         wbidirs.append( py4hw.FakeWire(f'rpi_gpio_tri_io[{i}]'))
+        
+    #     self.addInOut('rpi_gpio_tri_io', self.wire('rpi_gpio_tri_io', 32))
+        
+        
+    #     from py4hw.external.xilinx.io import TristateBuffer
+        
+    #     for i in range(20):
+    #         t = self.wire(f't{i}')
+    #         py4hw.Not(self, f't{i}', oes[i], t)
+    #         TristateBuffer(self, f'tbuf{i}', iis[i], t, os[i], wbidirs[i])
+            
+    #     return iis, os, oes
         
     def getVGAController(self, reset, vga_clk):
         '''
@@ -201,6 +258,37 @@ set_property -dict { PACKAGE_PIN J19   IOSTANDARD TMDS_33  } [get_ports { hdmi_t
 set_property -dict { PACKAGE_PIN K19   IOSTANDARD TMDS_33  } [get_ports { hdmi_tx_d_p[1] }]; #IO_L10P_T1_AD11P_35 Sch=hdmi_tx_d_p[1]
 set_property -dict { PACKAGE_PIN H18   IOSTANDARD TMDS_33  } [get_ports { hdmi_tx_d_n[2] }]; #IO_L14N_T2_AD4N_SRCC_35 Sch=hdmi_tx_d_n[2]
 set_property -dict { PACKAGE_PIN J18   IOSTANDARD TMDS_33  } [get_ports { hdmi_tx_d_p[2] }]; #IO_L14P_T2_AD4P_SRCC_35 Sch=hdmi_tx_d_p[2]
+
+##Raspberry Digital I/O
+
+set_property -dict { PACKAGE_PIN W18   IOSTANDARD LVCMOS33 } [get_ports { rpio_02_r }]; #IO_L22P_T3_34 Sch=rpio_02_r
+set_property -dict { PACKAGE_PIN W19   IOSTANDARD LVCMOS33 } [get_ports { rpio_03_r }]; #IO_L22N_T3_34 Sch=rpio_03_r
+set_property -dict { PACKAGE_PIN Y18   IOSTANDARD LVCMOS33 } [get_ports { rpio_04_r }]; #IO_L17P_T2_34 Sch=rpio_04_r
+set_property -dict { PACKAGE_PIN Y19   IOSTANDARD LVCMOS33 } [get_ports { rpio_05_r }]; #IO_L17N_T2_34 Sch=rpio_05_r
+set_property -dict { PACKAGE_PIN U18   IOSTANDARD LVCMOS33 } [get_ports { rpio_06_r }]; #IO_L22P_T3_13 Sch=rpio_06_r
+set_property -dict { PACKAGE_PIN U19   IOSTANDARD LVCMOS33 } [get_ports { rpio_07_r }]; #IO_L12P_T1_MRCC_34 Sch=rpio_07_r
+set_property -dict { PACKAGE_PIN F19   IOSTANDARD LVCMOS33 } [get_ports { rpio_08_r }]; #IO_L12N_T1_MRCC_34 Sch=rpio_08_r
+set_property -dict { PACKAGE_PIN V10   IOSTANDARD LVCMOS33 } [get_ports { rpio_09_r }]; #IO_L21N_T3_DQS_13 Sch=rpio_09_r
+set_property -dict { PACKAGE_PIN V8    IOSTANDARD LVCMOS33 } [get_ports { rpio_10_r }]; #IO_L15P_T2_DQS_13 Sch=rpio_10_r
+set_property -dict { PACKAGE_PIN W10   IOSTANDARD LVCMOS33 } [get_ports { rpio_11_r }]; #IO_L16P_T2_13 Sch=rpio_11_r
+set_property -dict { PACKAGE_PIN B20   IOSTANDARD LVCMOS33 } [get_ports { rpio_12_r }]; #IO_L1N_T0_AD0N_35 Sch=rpio_12_r
+set_property -dict { PACKAGE_PIN W8    IOSTANDARD LVCMOS33 } [get_ports { rpio_13_r }]; #IO_L15N_T2_DQS_13 Sch=rpio_13_r
+set_property -dict { PACKAGE_PIN V6    IOSTANDARD LVCMOS33 } [get_ports { rpio_14_r }]; #IO_L22P_T3_13 Sch=rpio_14_r
+set_property -dict { PACKAGE_PIN Y6    IOSTANDARD LVCMOS33 } [get_ports { rpio_15_r }]; #IO_L13N_T2_MRCC_13 Sch=rpio_15_r
+set_property -dict { PACKAGE_PIN B19   IOSTANDARD LVCMOS33 } [get_ports { rpio_16_r }]; #IO_L2P_T0_AD8P_35 Sch=rpio_16_r
+set_property -dict { PACKAGE_PIN U7    IOSTANDARD LVCMOS33 } [get_ports { rpio_17_r }]; #IO_L11P_T1_SRCC_13 Sch=rpio_17_r
+set_property -dict { PACKAGE_PIN C20   IOSTANDARD LVCMOS33 } [get_ports { rpio_18_r }]; #IO_L1P_T0_AD0P_35 Sch=rpio_18_r
+set_property -dict { PACKAGE_PIN Y8    IOSTANDARD LVCMOS33 } [get_ports { rpio_19_r }]; #IO_L14N_T2_SRCC_13 Sch=rpio_19_r
+set_property -dict { PACKAGE_PIN A20   IOSTANDARD LVCMOS33 } [get_ports { rpio_20_r }]; #IO_L2N_T0_AD8N_35 Sch=rpio_20_r
+set_property -dict { PACKAGE_PIN Y9    IOSTANDARD LVCMOS33 } [get_ports { rpio_21_r }]; #IO_L14P_T2_SRCC_13 Sch=rpio_21_r
+set_property -dict { PACKAGE_PIN U8    IOSTANDARD LVCMOS33 } [get_ports { rpio_22_r }]; #IO_L17N_T2_13 Sch=rpio_22_r
+set_property -dict { PACKAGE_PIN W6    IOSTANDARD LVCMOS33 } [get_ports { rpio_23_r }]; #IO_IO_L22N_T3_13 Sch=rpio_23_r
+set_property -dict { PACKAGE_PIN Y7    IOSTANDARD LVCMOS33 } [get_ports { rpio_24_r }]; #IO_L13P_T2_MRCC_13 Sch=rpio_24_r
+set_property -dict { PACKAGE_PIN F20   IOSTANDARD LVCMOS33 } [get_ports { rpio_25_r }]; #IO_L15N_T2_DQS_AD12N_35 Sch=rpio_25_r
+set_property -dict { PACKAGE_PIN W9    IOSTANDARD LVCMOS33 } [get_ports { rpio_26_r }]; #IO_L16N_T2_13 Sch=rpio_26_r
+set_property -dict { PACKAGE_PIN Y16   IOSTANDARD LVCMOS33 } [get_ports { rpio_sd_r }]; #IO_L7P_T1_34 Sch=rpio_sd_r
+set_property -dict { PACKAGE_PIN Y17   IOSTANDARD LVCMOS33 } [get_ports { rpio_sc_r }]; #IO_L7N_T1_34 Sch=rpio_sc_r
+
 
         '''
         return s
