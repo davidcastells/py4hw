9070add4453f88683463040a22db400f83f207dc
David Castells-Rufas <david.castells@uab.cat>
2026-09-17 06:05:45 +0530

    version 2026.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
                          
 setup(
     name='py4hw',
-    version='2026.4',
+    version='2026.6',
     author='David Castells-Rufas',
     author_email='david.castells@uab.cat',
     description='py4hw is a library to model, simulate, and execute digital logic circuits. It promotes the use of structural and behavioural design styles to build hardware.',
