9a670e407d3a6e3f1316dabfb6b1508749379b1c
David Castells-Rufas <david.castells@uab.cat>
2026-09-21 09:25:40 +0530

    PMODA connector

--- py4hw/external/platforms/tul.py
+++ py4hw/external/platforms/tul.py
@@ -59,6 +59,44 @@ class Pynq_Z2(py4hw.HWSystem):
 
         return key
 
+    def getPMODAInput(self, i):
+        
+        ii = self.wire(f'ja_ii{i}')
+        o = self.wire(f'ja_o{i}')
+        
+        wbidir = py4hw.FakeWire(f'ja[{i}]')
+        
+        if not('ja' in [p.name for p in self.inOutPorts]):
+            self.addInOut(f'ja', self.wire(f'w_ja', 8))
+                
+        from py4hw.external.xilinx.io import TristateBuffer
+        
+        t = self.wire(f'ja_t{i}')
+        py4hw.Constant(self, f'ja_t{i}', 1, t)
+        TristateBuffer(self, f'ja_tbuf{i}', ii, t, o, wbidir)
+            
+        return o
+
+    def getPMODAOutput(self, i):
+        
+        oe = self.wire(f'ja_oe{i}')
+        ii = self.wire(f'ja_ii{i}')
+        o = self.wire(f'ja_o{i}')
+        
+        wbidir = py4hw.FakeWire(f'ja[{i}]')
+
+        if not('ja' in [p.name for p in self.inOutPorts]):
+            self.addInOut(f'ja', self.wire(f'w_ja', 8))
+                
+        from py4hw.external.xilinx.io import TristateBuffer
+        
+        t = self.wire(f'ja_t{i}')
+        py4hw.Constant(self, f'ja_t{i}', 0, t)
+        TristateBuffer(self, f'ja_tbuf{i}', ii, t, o, wbidir)
+            
+        return ii
+
+
     def getRaspberryPIInput(self, i):
         
         ii = self.wire(f'ii{i}')
@@ -259,6 +297,28 @@ set_property -dict { PACKAGE_PIN K19   IOSTANDARD TMDS_33  } [get_ports { hdmi_t
 set_property -dict { PACKAGE_PIN H18   IOSTANDARD TMDS_33  } [get_ports { hdmi_tx_d_n[2] }]; #IO_L14N_T2_AD4N_SRCC_35 Sch=hdmi_tx_d_n[2]
 set_property -dict { PACKAGE_PIN J18   IOSTANDARD TMDS_33  } [get_ports { hdmi_tx_d_p[2] }]; #IO_L14P_T2_AD4P_SRCC_35 Sch=hdmi_tx_d_p[2]
 
+##PmodA
+
+set_property -dict { PACKAGE_PIN Y18   IOSTANDARD LVCMOS33 } [get_ports { ja[0] }]; #IO_L17P_T2_34 Sch=ja_p[1]
+set_property -dict { PACKAGE_PIN Y19   IOSTANDARD LVCMOS33 } [get_ports { ja[1] }]; #IO_L17N_T2_34 Sch=ja_n[1]
+set_property -dict { PACKAGE_PIN Y16   IOSTANDARD LVCMOS33 } [get_ports { ja[2] }]; #IO_L7P_T1_34 Sch=ja_p[2]
+set_property -dict { PACKAGE_PIN Y17   IOSTANDARD LVCMOS33 } [get_ports { ja[3] }]; #IO_L7N_T1_34 Sch=ja_n[2]
+set_property -dict { PACKAGE_PIN U18   IOSTANDARD LVCMOS33 } [get_ports { ja[4] }]; #IO_L12P_T1_MRCC_34 Sch=ja_p[3]
+set_property -dict { PACKAGE_PIN U19   IOSTANDARD LVCMOS33 } [get_ports { ja[5] }]; #IO_L12N_T1_MRCC_34 Sch=ja_n[3]
+set_property -dict { PACKAGE_PIN W18   IOSTANDARD LVCMOS33 } [get_ports { ja[6] }]; #IO_L22P_T3_34 Sch=ja_p[4]
+set_property -dict { PACKAGE_PIN W19   IOSTANDARD LVCMOS33 } [get_ports { ja[7] }]; #IO_L22N_T3_34 Sch=ja_n[4]
+
+##PmodB
+
+set_property -dict { PACKAGE_PIN W14   IOSTANDARD LVCMOS33 } [get_ports { jb[0] }]; #IO_L8P_T1_34 Sch=jb_p[1]
+set_property -dict { PACKAGE_PIN Y14   IOSTANDARD LVCMOS33 } [get_ports { jb[1] }]; #IO_L8N_T1_34 Sch=jb_n[1]
+set_property -dict { PACKAGE_PIN T11   IOSTANDARD LVCMOS33 } [get_ports { jb[2] }]; #IO_L1P_T0_34 Sch=jb_p[2]
+set_property -dict { PACKAGE_PIN T10   IOSTANDARD LVCMOS33 } [get_ports { jb[3] }]; #IO_L1N_T0_34 Sch=jb_n[2]
+set_property -dict { PACKAGE_PIN V16   IOSTANDARD LVCMOS33 } [get_ports { jb[4] }]; #IO_L18P_T2_34 Sch=jb_p[3]
+set_property -dict { PACKAGE_PIN W16   IOSTANDARD LVCMOS33 } [get_ports { jb[5] }]; #IO_L18N_T2_34 Sch=jb_n[3]
+set_property -dict { PACKAGE_PIN V12   IOSTANDARD LVCMOS33 } [get_ports { jb[6] }]; #IO_L4P_T0_34 Sch=jb_p[4]
+set_property -dict { PACKAGE_PIN W13   IOSTANDARD LVCMOS33 } [get_ports { jb[7] }]; #IO_L4N_T0_34 Sch=jb_n[4]
+
 ##Raspberry Digital I/O
 
 set_property -dict { PACKAGE_PIN W18   IOSTANDARD LVCMOS33 } [get_ports { rpio_02_r }]; #IO_L22P_T3_34 Sch=rpio_02_r
@@ -294,7 +354,12 @@ set_property -dict { PACKAGE_PIN Y17   IOSTANDARD LVCMOS33 } [get_ports { rpio_s
         return s
 
 
-    def build(self, projectDir):
+    def build(self, projectDir, delete_existing=False):
+        
+        if (delete_existing and os.path.exists(projectDir)):
+            import shutil
+            shutil.rmtree(projectDir)
+        
         if not os.path.exists(projectDir):
             print('Creating the directory', projectDir)
             os.makedirs(projectDir)
