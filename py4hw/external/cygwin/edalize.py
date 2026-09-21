a8bb63bdbfbb6a1c433519d17045bdf7a3f290b0
David Castells-Rufas <david.castells@uab.cat>
2026-09-21 09:24:38 +0530

    flag to avoid re-patching

--- py4hw/external/cygwin/edalize.py
+++ py4hw/external/cygwin/edalize.py
@@ -3,9 +3,12 @@ def patch_edalize_for_project(project_dir, VIVADO_PATH, BASH):
     import edalize.tools.vivado as vivado_tool
     import edalize.flows.edaflow as edaflow_mod
 
+    if (hasattr(vivado_tool.Vivado, 'py4hw_patch')):
+        print('Edalize was already patched for py4hw')
+        return
 
     def my_get_build_command(self):
-        print('BUILD OPTIONS:', self.build_options)
+        #print('BUILD OPTIONS:', self.build_options)
         return (BASH, ['-l', '-c', f'cd {project_dir};/bin/make']) 
 
 
@@ -15,9 +18,9 @@ def patch_edalize_for_project(project_dir, VIVADO_PATH, BASH):
     def _patched_setup(self, edam):
         _orig_setup(self, edam)
         for cmd in self.commands.commands:      # each is an EdaCommands.Command
-            print('CMD:', cmd)
+            #print('CMD:', cmd)
             for subcmd in cmd.commands:         # each is an argv-style list
-                print('SUBCMD:', subcmd)
+                #print('SUBCMD:', subcmd)
                 if subcmd and subcmd[0] == "vivado":
                     subcmd[0] = VIVADO_PATH
 
@@ -29,12 +32,15 @@ def patch_edalize_for_project(project_dir, VIVADO_PATH, BASH):
             full_cmd = f"cd {project_dir};/bin/make " + " ".join(args)
             kw["args"] = ["-l", "-c", full_cmd]
     
-            print('RUN:', BASH, a, kw)
+            #print('RUN:', BASH, a, kw)
             return _orig_run_tool(self, BASH, *a[1:] if a else (), **kw)
     
-        print('RUN:', cmd, a, kw)
+        #print('RUN:', cmd, a, kw)
         return _orig_run_tool(self, cmd, *a, **kw)
 
     vivado_tool.Vivado.setup = _patched_setup
     Make.get_build_command = my_get_build_command
     edaflow_mod.Edaflow._run_tool = patched_run_tool
+
+    vivado_tool.Vivado.py4hw_patch = True
+    
